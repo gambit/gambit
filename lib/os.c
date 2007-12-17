82f11b202af6eb6882f9562c8a8117ca56b6cfc6
Marc Feeley <feeley@iro.umontreal.ca>
2007-12-17 10:09:45 -0500

    [RUNTIME CHANGES NEEDED FOR v4.1.2] Changed version of runtime using misc/changev

--- lib/os.c
+++ lib/os.c
@@ -28,7 +28,7 @@
  */
 
 #define ___INCLUDED_FROM_OS
-#define ___VERSION 401001
+#define ___VERSION 401002
 #include "gambit.h"
 
 #include "os_base.h"
--- lib/os_base.c
+++ lib/os_base.c
@@ -7,7 +7,7 @@
  */
 
 #define ___INCLUDED_FROM_OS_BASE
-#define ___VERSION 401001
+#define ___VERSION 401002
 #include "gambit.h"
 
 #include "os_base.h"
