2b9b73c2b6471000e101603e190c956d157da982
Marc Feeley <feeley@iro.umontreal.ca>
2008-04-13 01:18:40 -0400

    [RUNTIME CHANGES NEEDED FOR v4.2.6] Changed version of runtime using misc/changev

--- lib/mem.c
+++ lib/mem.c
@@ -3,7 +3,7 @@
 /* Copyright (c) 1994-2007 by Marc Feeley, All Rights Reserved.  */
 
 #define ___INCLUDED_FROM_MEM
-#define ___VERSION 402005
+#define ___VERSION 402006
 #include "gambit.h"
 
 #include "os_base.h"
