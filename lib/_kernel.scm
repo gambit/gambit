01bc8e1b47eb1110d1cafd5aa7c710af71866448
Marc Feeley <feeley@iro.umontreal.ca>
2008-08-28 11:32:39 -0400

    Fixed incorrect computation by "time" macro of space allocated on 64 bit processors.

--- lib/_kernel.scm
+++ lib/_kernel.scm
@@ -1,6 +1,6 @@
 ;;;============================================================================
 
-;;; File: "_kernel.scm", Time-stamp: <2008-05-21 13:45:09 feeley>
+;;; File: "_kernel.scm", Time-stamp: <2008-08-28 11:28:33 feeley>
 
 ;;; Copyright (c) 1994-2008 by Marc Feeley, All Rights Reserved.
 
@@ -3699,59 +3699,43 @@ end-of-code
 
 (define-prim (##process-statistics)
   (##declare (not interrupts-enabled))
-  (let ((v
-         (##f64vector (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0)
-                      (macro-inexact-+0))))
+  (let ((v (##make-u8vector 168))) ;; max(4,8) + 20*sizeof(___F64)
+                                   ;; v must be a **movable** object
     (##c-code #<<end-of-code
 
      int n;
      ___F64 user, sys, real;
      long minflt, majflt;
+     ___SCMOBJ aligned_v = /* must align to turn u8vector to f64vector */
+       ___TAG(((___ARG1 - ___tSUBTYPED)+7)&~7,___tSUBTYPED);
+     ___HEADER(aligned_v) = ___MAKE_HD_BYTES(20*sizeof(___F64),___sF64VECTOR);
      ___W_ALL
      ___process_times (&user, &sys, &real);
      ___vm_stats (&minflt, &majflt);
-     ___F64VECTORSET(___ARG1,___FIX(0),user)
-     ___F64VECTORSET(___ARG1,___FIX(1),sys)
-     ___F64VECTORSET(___ARG1,___FIX(2),real)
-     ___F64VECTORSET(___ARG1,___FIX(3),___GSTATE->gc_user_time)
-     ___F64VECTORSET(___ARG1,___FIX(4),___GSTATE->gc_sys_time)
-     ___F64VECTORSET(___ARG1,___FIX(5),___GSTATE->gc_real_time)
-     ___F64VECTORSET(___ARG1,___FIX(6),___GSTATE->nb_gcs)
-     ___F64VECTORSET(___ARG1,___FIX(7),___bytes_allocated ())
-     ___F64VECTORSET(___ARG1,___FIX(8),(2*(1+2)<<___LWS))
-     n = (1<<___LWS) + 20 * sizeof (___F64) + (5<<___LWS);
-     ___F64VECTORSET(___ARG1,___FIX(9),(2*n))
-     ___F64VECTORSET(___ARG1,___FIX(10),minflt)
-     ___F64VECTORSET(___ARG1,___FIX(11),majflt)
-     ___F64VECTORSET(___ARG1,___FIX(12),___GSTATE->last_gc_user_time)
-     ___F64VECTORSET(___ARG1,___FIX(13),___GSTATE->last_gc_sys_time)
-     ___F64VECTORSET(___ARG1,___FIX(14),___GSTATE->last_gc_real_time)
-     ___F64VECTORSET(___ARG1,___FIX(15),___GSTATE->last_gc_heap_size)
-     ___F64VECTORSET(___ARG1,___FIX(16),___GSTATE->last_gc_alloc)
-     ___F64VECTORSET(___ARG1,___FIX(17),___GSTATE->last_gc_live)
-     ___F64VECTORSET(___ARG1,___FIX(18),___GSTATE->last_gc_movable)
-     ___F64VECTORSET(___ARG1,___FIX(19),___GSTATE->last_gc_nonmovable)
+     ___F64VECTORSET(aligned_v,___FIX(0),user)
+     ___F64VECTORSET(aligned_v,___FIX(1),sys)
+     ___F64VECTORSET(aligned_v,___FIX(2),real)
+     ___F64VECTORSET(aligned_v,___FIX(3),___GSTATE->gc_user_time)
+     ___F64VECTORSET(aligned_v,___FIX(4),___GSTATE->gc_sys_time)
+     ___F64VECTORSET(aligned_v,___FIX(5),___GSTATE->gc_real_time)
+     ___F64VECTORSET(aligned_v,___FIX(6),___GSTATE->nb_gcs)
+     ___F64VECTORSET(aligned_v,___FIX(7),___bytes_allocated ())
+     ___F64VECTORSET(aligned_v,___FIX(8),(2*(1+2)<<___LWS))
+     n = 8 + 20 * sizeof (___F64) + (5<<___LWS);
+     ___F64VECTORSET(aligned_v,___FIX(9),(2*n))
+     ___F64VECTORSET(aligned_v,___FIX(10),minflt)
+     ___F64VECTORSET(aligned_v,___FIX(11),majflt)
+     ___F64VECTORSET(aligned_v,___FIX(12),___GSTATE->last_gc_user_time)
+     ___F64VECTORSET(aligned_v,___FIX(13),___GSTATE->last_gc_sys_time)
+     ___F64VECTORSET(aligned_v,___FIX(14),___GSTATE->last_gc_real_time)
+     ___F64VECTORSET(aligned_v,___FIX(15),___GSTATE->last_gc_heap_size)
+     ___F64VECTORSET(aligned_v,___FIX(16),___GSTATE->last_gc_alloc)
+     ___F64VECTORSET(aligned_v,___FIX(17),___GSTATE->last_gc_live)
+     ___F64VECTORSET(aligned_v,___FIX(18),___GSTATE->last_gc_movable)
+     ___F64VECTORSET(aligned_v,___FIX(19),___GSTATE->last_gc_nonmovable)
      ___R_ALL
-     ___RESULT = ___ARG1;
-  
+     ___RESULT = aligned_v;
+
 end-of-code
 
      v)))
