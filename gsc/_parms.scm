1d8b281747823b96426af814f3433fec1a92d2ce
Marc Feeley <feeley@iro.umontreal.ca>
2008-11-27 09:27:48 -0500

    [COMPILER CHANGES NEEDED FOR v4.3.2] Changed version in compiler

--- gsc/_parms.scm
+++ gsc/_parms.scm
@@ -518,7 +518,7 @@
 
 ;; Gambit system version numbers.
 
-(define (compiler-version) 403001) ;; 100000*major + 1000*minor + revision
+(define (compiler-version) 403002) ;; 100000*major + 1000*minor + revision
 
 (define compiler-version-string-prefix "v")
 (define compiler-version-string-suffix "")
