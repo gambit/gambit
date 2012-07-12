921b06d93b3c0f54e967cbec3f8f67ab865fc7d5
Vincent Foley <vfoley@gmail.com>
2012-07-12 14:18:22 -0400

    Added a test to exercise x86 backend's handling of frame size

---
+++ gsc/tests/00-fixnum/funcall2.scm
@@ -0,0 +1,12 @@
+(define (test a b c d e f g h i j)
+  (test2 a c e g i))
+
+(define (test2 a b c d e)
+  (println a)
+  (println b)
+  (println c)
+  (println d)
+  (println e))
+
+
+(test 1 2 3 4 5 6 7 8 9 10)
