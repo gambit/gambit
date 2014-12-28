5922ff635085d925e372efadd5110bf0a84b897e
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-12-28 14:02:09 +0100

    SSL: support for client certificates (response to CertificateRequest message)

--- lib/os_io.c
+++ lib/os_io.c
@@ -4597,49 +4597,165 @@ int direction;)
 
 #ifdef USE_OPENSSL
 
-___HIDDEN int setup_ssl_connection
+/* Diffie-Hellman parameters */
+#ifndef OPENSSL_NO_DH
+  /* 1024-bit MODP Group with 160-bit prime order subgroup (RFC5114)
+   * -----BEGIN DH PARAMETERS-----
+   * MIIBDAKBgQCxC4+WoIDgHd6S3l6uXVTsUsmfvPsGo8aaap3KUtI7YWBz4oZ1oj0Y
+   * mDjvHi7mUsAT7LSuqQYRIySXXDzUm4O/rMvdfZDEvXCYSI6cIZpzck7/1vrlZEc4
+   * +qMaT/VbzMChUa9fDci0vUW/N982XBpl5oz9p21NpwjfH7K8LkpDcQKBgQCk0cvV
+   * w/00EmdlpELvuZkF+BBN0lisUH/WQGz/FCZtMSZv6h5cQVZLd35pD1UE8hMWAhe0
+   * sBuIal6RVH+eJ0n01/vX07mpLuGQnQ0iY/gKdqaiTAh6CR9THb8KAWm2oorWYqTR
+   * jnOvoy13nVkY0IvIhY9Nzvl8KiSFXm7rIrOy5QICAKA=
+   * -----END DH PARAMETERS-----
+   */
+  ___HIDDEN const unsigned char dh1024_p[]={
+    0xB1,0x0B,0x8F,0x96,0xA0,0x80,0xE0,0x1D,0xDE,0x92,0xDE,0x5E,
+    0xAE,0x5D,0x54,0xEC,0x52,0xC9,0x9F,0xBC,0xFB,0x06,0xA3,0xC6,
+    0x9A,0x6A,0x9D,0xCA,0x52,0xD2,0x3B,0x61,0x60,0x73,0xE2,0x86,
+    0x75,0xA2,0x3D,0x18,0x98,0x38,0xEF,0x1E,0x2E,0xE6,0x52,0xC0,
+    0x13,0xEC,0xB4,0xAE,0xA9,0x06,0x11,0x23,0x24,0x97,0x5C,0x3C,
+    0xD4,0x9B,0x83,0xBF,0xAC,0xCB,0xDD,0x7D,0x90,0xC4,0xBD,0x70,
+    0x98,0x48,0x8E,0x9C,0x21,0x9A,0x73,0x72,0x4E,0xFF,0xD6,0xFA,
+    0xE5,0x64,0x47,0x38,0xFA,0xA3,0x1A,0x4F,0xF5,0x5B,0xCC,0xC0,
+    0xA1,0x51,0xAF,0x5F,0x0D,0xC8,0xB4,0xBD,0x45,0xBF,0x37,0xDF,
+    0x36,0x5C,0x1A,0x65,0xE6,0x8C,0xFD,0xA7,0x6D,0x4D,0xA7,0x08,
+    0xDF,0x1F,0xB2,0xBC,0x2E,0x4A,0x43,0x71,
+  };
+  ___HIDDEN const unsigned char dh1024_g[]={
+    0xA4,0xD1,0xCB,0xD5,0xC3,0xFD,0x34,0x12,0x67,0x65,0xA4,0x42,
+    0xEF,0xB9,0x99,0x05,0xF8,0x10,0x4D,0xD2,0x58,0xAC,0x50,0x7F,
+    0xD6,0x40,0x6C,0xFF,0x14,0x26,0x6D,0x31,0x26,0x6F,0xEA,0x1E,
+    0x5C,0x41,0x56,0x4B,0x77,0x7E,0x69,0x0F,0x55,0x04,0xF2,0x13,
+    0x16,0x02,0x17,0xB4,0xB0,0x1B,0x88,0x6A,0x5E,0x91,0x54,0x7F,
+    0x9E,0x27,0x49,0xF4,0xD7,0xFB,0xD7,0xD3,0xB9,0xA9,0x2E,0xE1,
+    0x90,0x9D,0x0D,0x22,0x63,0xF8,0x0A,0x76,0xA6,0xA2,0x4C,0x08,
+    0x7A,0x09,0x1F,0x53,0x1D,0xBF,0x0A,0x01,0x69,0xB6,0xA2,0x8A,
+    0xD6,0x62,0xA4,0xD1,0x8E,0x73,0xAF,0xA3,0x2D,0x77,0x9D,0x59,
+    0x18,0xD0,0x8B,0xC8,0x85,0x8F,0x4D,0xCE,0xF9,0x7C,0x2A,0x24,
+    0x85,0x5E,0x6E,0xEB,0x22,0xB3,0xB2,0xE5,
+  };
+#endif
+
+
+___HIDDEN int setup_client_ssl_connection
    ___P((___device_tcp_client *dev),
         (dev)
 ___device_tcp_client *dev;)
 {
-
-  /* TODO: this initialization and configuration still
-   * needs to find the right place
-   */
-#define CERTF "foo-cert.pem"
-#define KEYF "foo-cert.pem"
-
-  static int initialized = 0;
-
   int err;
-  SSL_METHOD *ssl_method;
+  long ssl_options;
+  
+#if OPENSSL_VERSION_NUMBER >= 0x0090800fL
+#ifndef OPENSSL_NO_ECDH
+  EC_KEY *ecdh;
+  int nid;
+#endif
+#endif
 
-  if (!initialized)
+#ifndef OPENSSL_NO_DH
+  DH *dh;
+#endif
+
+  /* Reference:
+     https://github.com/lighttpd/lighttpd1.4/blob/master/src/network.c */
+
+  /**********************/
+  /* SSL Initialization */
+  static int SSL_initialized = 0;
+
+  if (!SSL_initialized)
     {      
       if (!SSL_library_init())
         {
-          fprintf(stderr, "** OpenSSL initialization failed!\n");
+          fprintf (stderr, "** OpenSSL initialization failed!\n");
           return ___FIX(___SSL_ERR);
         }
       SSL_load_error_strings();
+      OpenSSL_add_all_algorithms();
+      /* TODO: find the right place for application cleanup */
+      /* EVP_cleanup(); */
 
-      initialized = 1;
+      SSL_initialized = 1;
     }
-  /* END of initialization */
+  
+  /* Check Entropy */
+  if (!RAND_status())
+    {
+      fprintf(stderr, "** SSL: not enough entropy in the pool!\n");
+      return ___FIX(___SSL_ERR);
+    }
 
-  ssl_method = TLSv1_client_method();
+  /******************************/
+  /* SSL Context Initialization */
 
-  dev->ssl_ctx = SSL_CTX_new (ssl_method);
+/* SSL protocol bugs: workaround options
+   Ref: https://www.openssl.org/docs/ssl/SSL_CTX_set_options.html */
+#ifndef SSL_OP_NO_COMPRESSION
+#define SSL_OP_NO_COMPRESSION 0
+#endif
+  ssl_options =
+    SSL_OP_ALL |
+    SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION |
+    SSL_OP_NO_COMPRESSION;
+
+  dev->ssl_ctx = SSL_CTX_new (SSLv23_client_method());
   SSL_CHECK_ERROR (dev->ssl_ctx);
+
+  /* Public certificate and private key files and verification */
+  if (0 /* certificate */)
+    {
+      if (SSL_CTX_use_certificate_file (dev->ssl_ctx, "server.pem", SSL_FILETYPE_PEM) <= 0)
+        {
+          ERR_print_errors_fp(stderr);
+          return ___FIX(___SSL_ERR);
+        }
+      if (SSL_CTX_use_PrivateKey_file (dev->ssl_ctx, "server.pem", SSL_FILETYPE_PEM) <= 0)
+        {
+          ERR_print_errors_fp(stderr);
+          return ___FIX(___SSL_ERR);
+        }
+      if (SSL_CTX_check_private_key (dev->ssl_ctx) <= 0)
+        {
+          fprintf (stderr,"** SSL: Private key does not match the certificate public key\n");
+          return ___FIX(___SSL_ERR);
+        }
+    }
+
+  /* Required identifier for client certificate verification to work with sessions */
+  SSL_CHECK_ERROR (SSL_CTX_set_session_id_context (dev->ssl_ctx, "gambit", 6));
+
+  /* Option: re-activate empty fragments (countermeasure against BEAST attack) */
+  if (/*s->ssl_empty_fragments*/ 0 ) {
+#ifdef SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS
+    ssl_options &= ~SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS;
+#else
+    ssl_options &= ~0x00000800L; /* hardcode constant */
+    fprintf (stderr, "** SSL: SSL version doesn't support empty fragments\n");
+    return ___FIX(___SSL_ERR);
+#endif
+  }
+
+  SSL_CHECK_ERROR (ssl_options & SSL_CTX_set_options (dev->ssl_ctx, ssl_options));
+  /* No info callback */
+  SSL_CTX_set_info_callback (dev->ssl_ctx, NULL);
+
+  /* Force version >= TLS 1.0 */
+  SSL_CHECK_ERROR ((SSL_OP_NO_SSLv2 & SSL_CTX_set_options (dev->ssl_ctx, SSL_OP_NO_SSLv2)));
+  SSL_CHECK_ERROR ((SSL_OP_NO_SSLv3 & SSL_CTX_set_options (dev->ssl_ctx, SSL_OP_NO_SSLv3)));
+
+
+  SSL_CTX_set_mode (dev->ssl_ctx,
+                      SSL_MODE_ENABLE_PARTIAL_WRITE |
+                      SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER);
+
+  /*********************************/
+  /* SSL Connection Initialization */
 
   dev->ssl  = SSL_new (dev->ssl_ctx);
   SSL_set_fd (dev->ssl, dev->s);
 
-  SSL_set_mode (dev->ssl,
-                SSL_MODE_ENABLE_PARTIAL_WRITE |
-                SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER |
-                SSL_MODE_RELEASE_BUFFERS);
-
+  
   return 0;
 }
 
@@ -4696,7 +4812,7 @@ int direction;)
   *dev = d;
 
 #ifdef USE_OPENSSL
-  setup_ssl_connection (d);
+  setup_client_ssl_connection (d);
 #endif
 
   if (try_connect (d) != 0)
