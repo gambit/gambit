f67d29a4f71ce776684fa832608fcb32bacae5ac
Bradley Lucier <lucier@math.purdue.edu>
2015-03-04 16:26:24 -0500

    Turn ##wr-meroon and ##wr-jazz into hooks that can be overridden later

--- lib/_io.scm
+++ lib/_io.scm
@@ -9619,12 +9619,16 @@
    'meroon
    (##void)))
 
+(set! ##wr-meroon ##wr-meroon)
+
 (define-prim (##wr-jazz we obj)
   (##wr-sn
    we
    obj
    'jazz
    (##void)))
+
+(set! ##wr-jazz ##wr-jazz)
 
 (define-prim (##wr-frame we obj)
   (if (##eq? (macro-readtable-sharing-allowed?
