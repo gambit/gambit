f24f8914b28fa6d7ce8dc86ba1ce0a0c7c6e1ad7
Bradley Lucier <lucier@math.purdue.edu>
2015-03-09 22:16:22 -0400

    Add some tests for complex asin and acos

--- tests/unit-tests/03-number/acos.scm
+++ tests/unit-tests/03-number/acos.scm
@@ -13,6 +13,14 @@
 (check-= (acos -2+0.i) (test-acos -2+0.i))
 (check-= (acos -2-0.i) (test-acos -2-0.i))
 
+;;; Not all implementations agree on these.
+
+;;; The vagaries of complex multiplication and signed zeros make
+;;; the "naive" test-acos give an incorrect answer.
+
+;; (check-= (acos 1234000000.+0.i) (test-acos 1234000000.+0.i))
+(check-= (acos -1234000000.-0.i) (test-acos -1234000000.-0.i))
+
 ;;; Test exceptions
 
 (check-tail-exn type-exception? (lambda () (acos 'a)))
