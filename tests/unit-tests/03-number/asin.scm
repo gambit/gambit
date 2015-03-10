d4fdeb6f8fc26db62e3e754a0c722811c1616729
Bradley Lucier <lucier@math.purdue.edu>
2015-03-09 22:16:22 -0400

    Add some tests for complex asin and acos

--- tests/unit-tests/03-number/asin.scm
+++ tests/unit-tests/03-number/asin.scm
@@ -17,6 +17,14 @@
 
 (check-eqv? (asin 1e-30+1e-40i) 1e-30+1e-40i)
 
+;;; Not all implementations agree on these.
+
+;;; The vagaries of complex multiplication and signed zeros make
+;;; the "naive" test-asin give an incorrect answer.
+
+;; (check-= (asin 1234000000.+0.i) (test-asin 1234000000.+0.i))
+(check-= (asin -1234000000.-0.i) (test-asin -1234000000.-0.i))
+
 ;;; Test exceptions
 
 (check-tail-exn type-exception? (lambda () (asin 'a)))
