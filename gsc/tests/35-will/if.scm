c50d8bbbdb31b268326a81f8910f9438a3eabd53
astlouisf <astlouisf@gmail.com>
2015-07-24 05:36:14 -0400

    Add will primitives tests.

---
+++ gsc/tests/35-will/if.scm
@@ -0,0 +1,10 @@
+(declare (extended-bindings) (not constant-fold) (not safe))
+
+(define v1 (##make-will 1 (lambda (x) x)))
+(define v2 (##make-will 2 (lambda (x) x)))
+
+(define (test x)
+  (println (if x 11 22)))
+
+(test v1)
+(test v2)
