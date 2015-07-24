35f29c542edd09ec825ea4622a7c268a4a622962
astlouisf <astlouisf@gmail.com>
2015-07-24 05:28:13 -0400

    Add promise primitives tests.
    Conflicts:
    	include/stamp.h

---
+++ gsc/tests/34-promise/eq.scm
@@ -0,0 +1,15 @@
+(declare (extended-bindings) (not constant-fold) (not safe))
+
+(define v1 (##make-promise (lambda () 1)))
+(define v2 (##make-promise (lambda () 2)))
+
+(define (test2 x y)
+  (println (##eq? x y))
+  (println (if (##eq? x y) 11 22)))
+
+(define (test x)
+  (test2 x v1)
+  (test2 x v2))
+
+(test v1)
+(test v2)
