b9410709a81ee3bec4d4d249670771a4483e7197
James Baker <jamesb@ionata.com.au>
2016-02-04 08:12:07 +1100

    fxzerop test

---
+++ tests/unit-tests/01-fixnum/fxzerop.scm
@@ -0,0 +1,10 @@
+(include "#.scm")
+
+(check-eqv? (##fxzero? 0)   #t)
+(check-eqv? (##fxzero? 1)   #f)
+
+(check-eqv? (fxzero? 0)   #t)
+(check-eqv? (fxzero? 1)   #f)
+
+(check-tail-exn type-exception? (lambda () (fxzero? 0.5)))
+(check-tail-exn type-exception? (lambda () (fxzero? 1/3)))
