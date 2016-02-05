357836e6132b7eaf2559618a68baf4327a5ac597
James Baker <jamesb@ionata.com.au>
2016-02-06 10:23:38 +1100

    unit test: fxwrapabs

---
+++ tests/unit-tests/01-fixnum/fxwrapabs.scm
@@ -0,0 +1,19 @@
+(include "#.scm")
+
+(check-eqv? (##fxwrapabs -1) 1)
+(check-eqv? (##fxwrapabs 0) 0)
+(check-eqv? (##fxwrapabs 1) 1)
+(check-eqv? (##fxwrapabs 1000) 1000)
+(check-eqv? (##fxwrapabs -1000) 1000)
+
+(check-eqv? (fxwrapabs -1) 1)
+(check-eqv? (fxwrapabs 0) 0)
+(check-eqv? (fxwrapabs 1) 1)
+(check-eqv? (fxwrapabs 1000) 1000)
+(check-eqv? (fxwrapabs -1000) 1000)
+
+(check-tail-exn wrong-number-of-arguments-exception? (lambda () (fxwrapabs)))
+(check-tail-exn wrong-number-of-arguments-exception? (lambda () (fxwrapabs 1 2)))
+
+(check-tail-exn type-exception? (lambda () (fxwrapabs 0.5)))
+(check-tail-exn type-exception? (lambda () (fxwrapabs 1/2)))
