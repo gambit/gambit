68c65fe98f12de8ecc497b4d58af857cda6ea25a
James Baker <jamesb@ionata.com.au>
2016-02-06 10:26:23 +1100

    unit test: fx<

---
+++ tests/unit-tests/01-fixnum/fxlt.scm
@@ -0,0 +1,14 @@
+(include "#.scm")
+
+(check-eqv? (##fx<) #t)
+(check-eqv? (##fx< 1) #t)
+(check-eqv? (##fx< ##max-fixnum ##min-fixnum) #f)
+(check-eqv? (##fx< ##min-fixnum ##max-fixnum) #t)
+
+(check-eqv? (fx<) #t)
+(check-eqv? (fx< 1) #t)
+(check-eqv? (fx< ##max-fixnum ##min-fixnum) #f)
+(check-eqv? (fx< ##min-fixnum ##max-fixnum) #t)
+
+(check-tail-exn type-exception? (lambda () (fx< 1 0.5)))
+(check-tail-exn type-exception? (lambda () (fx< 1/3 1)))
