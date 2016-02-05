74ea6bcab10f8e01bc10cdd729b75754d23adb0b
James Baker <jamesb@ionata.com.au>
2016-02-06 09:55:12 +1100

    unit test: fxnegative?

---
+++ tests/unit-tests/01-fixnum/fxnegativep.scm
@@ -0,0 +1,13 @@
+(include "#.scm")
+
+(check-eqv? (##fxnegative? ##min-fixnum) #t)
+(check-eqv? (##fxnegative? ##max-fixnum) #f)
+
+(check-eqv? (fxnegative? ##min-fixnum) #t)
+(check-eqv? (fxnegative? ##max-fixnum) #f)
+
+(check-tail-exn type-exception? (lambda () (fxnegative? 0.5)))
+(check-tail-exn type-exception? (lambda () (fxnegative? 1/2)))
+
+(check-tail-exn wrong-number-of-arguments-exception? (lambda () (fxnegative?)))
+(check-tail-exn wrong-number-of-arguments-exception? (lambda () (fxnegative? 2 -4)))
