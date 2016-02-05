977e72d157f071c12d62d3699bf1c6ecff426d56
James Baker <jamesb@ionata.com.au>
2016-02-06 10:21:47 +1100

    unit test: fxquotient

---
+++ tests/unit-tests/01-fixnum/fxquotient.scm
@@ -0,0 +1,17 @@
+(include "#.scm")
+
+(check-eqv? (##fxquotient 1 1) 1)
+(check-eqv? (##fxquotient 2 1) 2)
+(check-eqv? (##fxquotient 3 1) 3)
+
+(check-eqv? (fxquotient 1 1) 1)
+(check-eqv? (fxquotient 2 1) 2)
+(check-eqv? (fxquotient 3 1) 3)
+
+(check-tail-exn divide-by-zero-exception? (lambda () (fxquotient 1 0)))
+
+(check-tail-exn type-exception? (lambda () (fxquotient 1 0.5)))
+(check-tail-exn type-exception? (lambda () (fxquotient 1 1/2)))
+
+(check-tail-exn wrong-number-of-arguments-exception? (lambda () (fxquotient)))
+(check-tail-exn wrong-number-of-arguments-exception? (lambda () (fxquotient 1 1 1)))
