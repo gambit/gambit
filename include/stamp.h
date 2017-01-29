a8039701e13377ecd554a3317e9c9741a5c85705
Marc Feeley <feeley@iro.umontreal.ca>
2017-01-29 10:09:52 -0500

    Fix incomplete merge of include/stamp.h

--- include/stamp.h
+++ include/stamp.h
@@ -3,8 +3,4 @@
  */
 
 #define ___STAMP_YMD 20170129
-<<<<<<< HEAD
-#define ___STAMP_HMS 125648
-=======
 #define ___STAMP_HMS 142848
->>>>>>> 0209073... Fix build process so that it can build multiple branches
