5d93fad6cb221c2556d1fb1f7b7abba66819081d
Nicolas Hurtubise <316k@legtux.org>
2017-05-05 15:38:17 -0400

    Disables ##fxsquarep tests with an arg bigger than max-fixnum

--- tests/unit-tests/01-fixnum/fxsquarep.scm
+++ tests/unit-tests/01-fixnum/fxsquarep.scm
@@ -7,7 +7,10 @@
 (check-eqv? (##fxsquare? -23170) 536848900)
 (check-eqv? (##fxsquare?  23171) (and (fixnum? 536895241) 536895241))
 (check-eqv? (##fxsquare? -23171) (and (fixnum? 536895241) 536895241))
-(check-eqv? (##fxsquare?  1518500249) (and (fixnum? 2305843006213062001) 2305843006213062001))
-(check-eqv? (##fxsquare?  -1518500249) (and (fixnum? 2305843006213062001) 2305843006213062001))
-(check-eqv? (##fxsquare?  1518500250) #f)
-(check-eqv? (##fxsquare? -1518500250) #f)
+
+(if (< 1518500250 ##max-fixnum)
+    (begin
+      (check-eqv? (##fxsquare?  1518500249) (and (fixnum? 2305843006213062001) 2305843006213062001))
+      (check-eqv? (##fxsquare?  -1518500249) (and (fixnum? 2305843006213062001) 2305843006213062001))
+      (check-eqv? (##fxsquare?  1518500250) #f)
+      (check-eqv? (##fxsquare? -1518500250) #f)))
