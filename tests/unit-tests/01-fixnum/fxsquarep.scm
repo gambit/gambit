f0524289463b6d0a85a1f843a7fe238160bf3cfd
Nicolas Hurtubise <316k@legtux.org>
2017-05-09 12:09:07 -0400

    Use (fixnum? ...) instead of (< ... ##max-fixnum) in the ##fxsquare? test

--- tests/unit-tests/01-fixnum/fxsquarep.scm
+++ tests/unit-tests/01-fixnum/fxsquarep.scm
@@ -8,9 +8,12 @@
 (check-eqv? (##fxsquare?  23171) (and (fixnum? 536895241) 536895241))
 (check-eqv? (##fxsquare? -23171) (and (fixnum? 536895241) 536895241))
 
-(if (< 1518500250 ##max-fixnum)
+(if (fixnum? 1518500250)
     (begin
       (check-eqv? (##fxsquare?  1518500249) (and (fixnum? 2305843006213062001) 2305843006213062001))
+      (check-eqv? (##fxsquare?  1518500250) #f)))
+
+(if (fixnum? -1518500250)
+    (begin
       (check-eqv? (##fxsquare?  -1518500249) (and (fixnum? 2305843006213062001) 2305843006213062001))
-      (check-eqv? (##fxsquare?  1518500250) #f)
       (check-eqv? (##fxsquare? -1518500250) #f)))
