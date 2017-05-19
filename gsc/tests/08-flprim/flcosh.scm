392ccef29de60c53db904e2310e1c316db3d5e4d
Nicolas Hurtubise <316k@legtux.org>
2017-05-18 20:45:00 -0400

    Adds more unit tests for float primitives

---
+++ gsc/tests/08-flprim/flcosh.scm
@@ -0,0 +1,14 @@
+(declare (extended-bindings) (not constant-fold) (not safe))
+
+(define (test a b)
+  (let ((x (##flcosh a)))
+    (println (and (##fl>= x (##fl* 0.999999999999999 b))
+                  (##fl<= x (##fl* 1.000000000000001 b))))))
+
+
+(test  0.0 1.0)
+(test -0.0 1.0)
+(test  0.1 1.0050041680558035)
+(test  1.0 1.5430806348152437)
+(test -1.0 1.5430806348152437)
+(test 10.0 11013.232920103328)
---
+++ gsc/tests/08-flprim/flsinh.scm
@@ -0,0 +1,14 @@
+(declare (extended-bindings) (not constant-fold) (not safe))
+
+(define (test a b)
+  (let ((x (##flsinh a)))
+    (println (and (##fl>= x (##fl* 0.999999999999999 b))
+                  (##fl<= x (##fl* 1.000000000000001 b))))))
+
+
+(println (##eqv? (##flsinh 0.0) 0.0))
+(println (##eqv? (##flsinh -0.0) -0.0))
+
+(test 0.1  0.10016675001984403)
+(test 1.0  1.1752011936438014)
+(test 10.0 11013.232874703397)
