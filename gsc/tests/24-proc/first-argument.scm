c6520d13023e8077756c190a0e01eaaabdc39c9c
Marc Feeley <feeley@iro.umontreal.ca>
2017-05-19 06:55:17 -0400

    Universal Backend: fix unit tests for ##apply and ##first-argument

--- gsc/tests/24-proc/first-argument.scm
+++ gsc/tests/24-proc/first-argument.scm
@@ -1,7 +1,8 @@
 (declare (extended-bindings) (not constant-fold) (not safe))
 
-(println (##= (##first-argument 1 1) 1))
-(println (##= (##first-argument -1000 2000 3000) -1000))
-(println (##= (##first-argument -1000.9293221 #t #f 10 20 30 '(1 2 3 4 5) '(2 . 45)) -1000.9293221))
-(println (##eq? (##first-argument ##+ ##- ##* ##/) ##+))
-(println (##= (##car (##first-argument '(1 2 3 4 5))) 1))
+(println (##first-argument 42))
+(println (##first-argument 1 2))
+(println (##first-argument -1000 2000 3000))
+(println (##first-argument -1.5 #t #f 10 20 30 '(1 2 3 4 5) '(2 . 45)))
+(println (##car (##first-argument '(1 2 3 4 5) "second")))
+(println (##eq? (##first-argument println 1 2 3 4) println))
