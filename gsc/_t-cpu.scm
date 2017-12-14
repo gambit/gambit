d2af2c08e9803f87bdab7259b04e1e19de556098
Marc Feeley <feeley@iro.umontreal.ca>
2017-12-14 12:34:03 -0500

    Add a CPU target to support X86 and ARM

---
+++ gsc/_t-cpu.scm
@@ -0,0 +1,268 @@
+;;============================================================================
+
+;;; File: "_t-cpu.scm"
+
+;;; Copyright (c) 2011-2017 by Marc Feeley, All Rights Reserved.
+
+(include "generic.scm")
+
+(include-adt "_envadt.scm")
+(include-adt "_gvmadt.scm")
+(include-adt "_ptreeadt.scm")
+(include-adt "_sourceadt.scm")
+
+;;;----------------------------------------------------------------------------
+;;
+;; "CPU" back-end that targets hardware processors.
+
+;; Initialization/finalization of back-end.
+
+(define (cpu-setup
+         target-arch
+         file-extensions
+         max-nb-gvm-regs
+         default-nb-gvm-regs
+         default-nb-arg-regs
+         semantics-changing-options
+         semantics-preserving-options)
+
+  (define common-semantics-changing-options
+    '())
+
+  (define common-semantics-preserving-options
+    '((asm symbol)))
+
+  (let ((targ
+         (make-target 12
+                      target-arch
+                      file-extensions
+                      (append semantics-changing-options
+                              common-semantics-changing-options)
+                      (append semantics-preserving-options
+                              common-semantics-preserving-options)
+                      0)))
+
+    (define (begin! sem-changing-opts
+                    sem-preserving-opts
+                    info-port)
+
+      (target-dump-set!
+       targ
+       (lambda (procs output c-intf module-descr unique-name)
+         (cpu-dump targ
+                   procs
+                   output
+                   c-intf
+                   module-descr
+                   unique-name
+                   sem-changing-opts
+                   sem-preserving-opts)))
+
+      (target-link-info-set!
+       targ
+       (lambda (file)
+         (cpu-link-info targ file)))
+
+      (target-link-set!
+       targ
+       (lambda (extension? inputs output warnings?)
+         (cpu-link targ extension? inputs output warnings?)))
+
+      (target-prim-info-set! targ cpu-prim-info)
+
+      (target-frame-constraints-set!
+       targ
+       (make-frame-constraints
+        cpu-frame-reserve
+        cpu-frame-alignment))
+
+      (target-proc-result-set!
+       targ
+       (make-reg 1))
+
+      (target-task-return-set!
+       targ
+       (make-reg 0))
+
+      (target-switch-testable?-set!
+       targ
+       (lambda (obj)
+         (cpu-switch-testable? targ obj)))
+
+      (target-eq-testable?-set!
+       targ
+       (lambda (obj)
+         (cpu-eq-testable? targ obj)))
+
+      (target-object-type-set!
+       targ
+       (lambda (obj)
+         (cpu-object-type targ obj)))
+
+      (cpu-set-nb-regs targ sem-changing-opts max-nb-gvm-regs)
+
+      #f)
+
+    (define (end!)
+      #f)
+
+    (target-begin!-set! targ begin!)
+    (target-end!-set! targ end!)
+    (target-add targ)))
+
+(cpu-setup 'x86-32 '((".s" . X86-32))  5 5 3 '() '())
+(cpu-setup 'x86-64 '((".s" . X86-64)) 13 5 3 '() '())
+(cpu-setup 'arm    '((".s" . ARM))    13 5 3 '() '())
+
+;;;----------------------------------------------------------------------------
+
+;; ***** REGISTERS AVAILABLE
+
+;; The registers available in the virtual machine default to
+;; cpu-default-nb-gvm-regs and cpu-default-nb-arg-regs but can be
+;; changed with the gsc options -nb-gvm-regs and -nb-arg-regs.
+;;
+;; nb-gvm-regs = total number of registers available
+;; nb-arg-regs = maximum number of arguments passed in registers
+
+(define cpu-default-nb-gvm-regs 5)
+(define cpu-default-nb-arg-regs 3)
+
+(define (cpu-set-nb-regs targ sem-changing-opts max-nb-gvm-regs)
+  (let ((nb-gvm-regs
+         (get-option sem-changing-opts
+                     'nb-gvm-regs
+                     cpu-default-nb-gvm-regs))
+        (nb-arg-regs
+         (get-option sem-changing-opts
+                     'nb-arg-regs
+                     cpu-default-nb-arg-regs)))
+
+    (if (not (and (<= 3 nb-gvm-regs)
+                  (<= nb-gvm-regs max-nb-gvm-regs)))
+        (compiler-error
+         (string-append "-nb-gvm-regs option must be between 3 and "
+                        (number->string max-nb-gvm-regs))))
+
+    (if (not (and (<= 1 nb-arg-regs)
+                  (<= nb-arg-regs (- nb-gvm-regs 2))))
+        (compiler-error
+         (string-append "-nb-arg-regs option must be between 1 and "
+                        (number->string (- nb-gvm-regs 2)))))
+
+    (target-nb-regs-set! targ nb-gvm-regs)
+    (target-nb-arg-regs-set! targ nb-arg-regs)))
+
+;;;----------------------------------------------------------------------------
+
+;; The frame constraints are defined by the parameters
+;; cpu-frame-reserve and cpu-frame-alignment.
+
+(define cpu-frame-reserve 0) ;; no extra slots reserved
+(define cpu-frame-alignment 1) ;; no alignment constraint
+
+;;;----------------------------------------------------------------------------
+
+;; ***** PROCEDURE CALLING CONVENTION
+
+(define (cpu-label-info targ nb-params closed?)
+  ((target-label-info targ) nb-params closed?))
+
+(define (cpu-jump-info targ nb-args)
+  ((target-jump-info targ) nb-args))
+
+;;;----------------------------------------------------------------------------
+
+;; ***** PRIMITIVE PROCEDURE DATABASE
+
+(define cpu-prim-proc-table
+  (let ((t (make-prim-proc-table)))
+    (for-each
+     (lambda (x) (prim-proc-add! t x))
+     '())
+    t))
+
+(define (cpu-prim-info name)
+  (prim-proc-info cpu-prim-proc-table name))
+
+(define (cpu-get-prim-info name)
+  (let ((proc (cpu-prim-info (string->canonical-symbol name))))
+    (if proc
+        proc
+        (compiler-internal-error
+         "cpu-get-prim-info, unknown primitive:" name))))
+
+;;;----------------------------------------------------------------------------
+
+;; ***** OBJECT PROPERTIES
+
+(define (cpu-switch-testable? targ obj)
+  ;;(pretty-print (list 'cpu-switch-testable? 'targ obj))
+  #f);;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (cpu-eq-testable? targ obj)
+  ;;(pretty-print (list 'cpu-eq-testable? 'targ obj))
+  #f);;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (cpu-object-type targ obj)
+  ;;(pretty-print (list 'cpu-object-type 'targ obj))
+  'bignum);;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;----------------------------------------------------------------------------
+
+;; ***** LINKING
+
+(define (cpu-link-info file)
+  (pretty-print (list 'cpu-link-info file))
+  #f)
+
+(define (cpu-link extension? inputs output warnings?)
+  (pretty-print (list 'cpu-link extension? inputs output warnings?))
+  #f)
+
+;;;----------------------------------------------------------------------------
+
+;; ***** DUMPING OF A COMPILATION MODULE
+
+(define (cpu-dump targ
+                  procs
+                  output
+                  c-intf
+                  module-descr
+                  unique-name
+                  sem-changing-options
+                  sem-preserving-options)
+
+  (pretty-print (list 'cpu-dump
+                      (target-name targ)
+                      (map proc-obj-name procs)
+                      output
+                      unique-name))
+
+  ;; for now just display the GVM code
+  (virtual.dump-gvm procs (current-output-port))
+
+  #f)
+
+;;;----------------------------------------------------------------------------
+
+;; ***** INLINING OF PRIMITIVES
+
+(define (cpu-inlinable name)
+  (let ((prim (cpu-get-prim-info name)))
+    (proc-obj-inlinable?-set! prim (lambda (env) #t))))
+
+(define (cpu-testable name)
+  (let ((prim (cpu-get-prim-info name)))
+    (proc-obj-testable?-set! prim (lambda (env) #t))))
+
+(define (cpu-jump-inlinable name)
+  (let ((prim (cpu-get-prim-info name)))
+    (proc-obj-jump-inlinable?-set! prim (lambda (env) #t))))
+
+(cpu-inlinable "##fx+")
+(cpu-inlinable "##fx-")
+
+(cpu-testable "##fx<")
+
+;;;============================================================================
