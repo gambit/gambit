719586e72541e4cc47c12a9755822632d716a8e9
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2019-06-09 22:58:28 -0400

    RISC-V Backend: Including some pseudoinstructions

--- gsc/_riscv.scm
+++ gsc/_riscv.scm
@@ -33,11 +33,11 @@
 
 (define-macro (riscv-assert-64bit-mode cgc)
   `(assert (riscv-64bit-mode? ,cgc)
-           "instruction only valid for RV64I"))
+    "instruction only valid for RV64I"))
 
 (define-macro (riscv-assert-32bit-mode cgc)
   `(assert (not (riscv-64bit-mode? ,cgc))
-           "instruction only valid for RV32I"))
+    "instruction only valid for RV32I"))
 
 ;;;----------------------------------------------------------------------------
 
@@ -167,9 +167,9 @@
                           cgc
                           (list #\tab
                                 (cond ((fx= width 8)  ".byte")
-                                      ((fx= width 16) ".2byte")
-                                      ((fx= width 32) ".4byte")
-                                      ((fx= width 64) ".8byte")
+                                      ((fx= width 16) ".half")
+                                      ((fx= width 32) ".word")
+                                      ((fx= width 64) ".dword")
                                       (else (error "unknown data width")))
                                 #\tab
                                 (asm-separated-list (reverse rev-lst) ","))))
@@ -177,7 +177,88 @@
 
 ;;;----------------------------------------------------------------------------
 
-;;; TODO Pseudo instructions.
+;;; XXX Pseudo instructions.
+
+; (define (riscv-la cgc rd symbol))
+; (define (riscv-lb cgc rd symbol))
+; (define (riscv-lh cgc rd symbol))
+; (define (riscv-lw cgc rd symbol))
+; (define (riscv-ld cgc rd symbol))
+; (define (riscv-sb cgc rd symbol rt))
+; (define (riscv-sh cgc rd symbol rt))
+; (define (riscv-sw cgc rd symbol rt))
+; (define (riscv-sd cgc rd symbol rt))
+
+(define (riscv-nop cgc)
+  (riscv-addi cgc (riscv-x0) (riscv-x0) (riscv-imm-int 0)))
+; (define (riscv-li cgc rd immediate))
+(define (riscv-mv cgc rd rs)
+  (riscv-addi cgc rd rs (riscv-imm-int 0)))
+(define (riscv-not cgc rd rs)
+  (riscv-xori cgc rd rs (riscv-imm-int -1)))
+(define (riscv-neg cgc rd rs)
+  (riscv-sub cgc rd (riscv-x0) rs))
+(define (riscv-negw cgc rd rs)
+  (riscv-subw cgc rd (riscv-x0) rs))
+(define (riscv-sext.w cgc rd rs)
+  (riscv-addiw cgc rd rs (riscv-imm-int 0)))
+(define (riscv-seqz cgc rd rs)
+  (riscv-sltiu cgc rd rs (riscv-imm-int 1)))
+(define (riscv-snez cgc rd rs)
+  (riscv-sltu cgc rd (riscv-x0) rs))
+(define (riscv-sltz cgc rd rs)
+  (riscv-slt cgc rd rs (riscv-x0)))
+(define (riscv-sgtz cgc rd rs)
+  (riscv-slt cgc rd (riscv-x0) rs))
+
+(define (riscv-beqz cgc rs offset)
+  (riscv-beq cgc rs (riscv-x0) offset))
+(define (riscv-bnez cgc rs offset)
+  (riscv-bne cgc rs (riscv-x0) offset))
+(define (riscv-blez cgc rs offset)
+  (riscv-bge cgc (riscv-x0) rs offset))
+(define (riscv-bgez cgc rs offset)
+  (riscv-bge cgc rs (riscv-x0) offset))
+(define (riscv-bltz cgc rs offset)
+  (riscv-blt cgc rs (riscv-x0) offset))
+(define (riscv-bgtz cgc rs offset)
+  (riscv-blt cgc (riscv-x0) rs offset))
+
+(define (riscv-bgt cgc rs rt offset)
+  (riscv-blt cgc rt rs offset))
+(define (riscv-ble cgc rs rt offset)
+  (riscv-bge cgc rt rs offset))
+(define (riscv-bgtu cgc rs rt offset)
+  (riscv-bltu cgc rt rs offset))
+(define (riscv-bleu cgc rs rt offset)
+  (riscv-bgeu cgc rt rs offset))
+
+(define (riscv-j cgc offset)
+  (riscv-jal cgc (riscv-x0) offset))
+; (define (riscv-jal cgc offset)
+;   (riscv-jal cgc (riscv-x1) offset))
+(define (riscv-jr cgc rs)
+  (riscv-jalr cgc (riscv-x0) rs (riscv-imm-int 0)))
+; (define (riscv-jalr cgc rs)
+;   (riscv-jalr cgc (riscv-x1) rs (riscv-imm-int 0)))
+(define (riscv-ret cgc)
+  (riscv-jalr cgc (riscv-x0) (riscv-x1) (riscv-imm-int 0)))
+; XXX Repetition
+(define (riscv-call cgc offset)
+  (riscv-auipc cgc (riscv-x6)
+               (riscv-imm-int (fxand (riscv-imm-int-value offset) #xfffff000) 'U)) ; XXX
+  (riscv-jalr cgc (riscv-x1) (riscv-x6)
+              (riscv-imm-int (fxand (riscv-imm-int-value offset) #x00000fff) 'I))) ; XXX
+(define (riscv-tail cgc offset)
+  (riscv-auipc cgc (riscv-x6)
+               (riscv-imm-int (fxand (riscv-imm-int-value offset) #xfffff000) 'U)) ; XXX
+  (riscv-jalr cgc (riscv-x0) (riscv-x6)
+              (riscv-imm-int (fxand (riscv-imm-int-value offset) #x00000fff) 'I))) ; XXX
+
+; (define (riscv-fence cgc)
+;   (riscv-fence cgc #b1111 #b1111))
+
+;; TODO Pseudoinstructions for accessing control and status registers
 
 ;;;----------------------------------------------------------------------------
 
@@ -310,17 +391,17 @@
 (define (riscv-andi cgc rd rs1 imm)
   (riscv-type-i cgc rd rs1 imm #x7000))
 
-(define (riscv-slli cgc rd rs1 shamt)
+(define (riscv-slli cgc rd rs1 shamt) ; XXX
   (assert (and (fx>= shamt 0) (fx< shamt (riscv-word-width cgc)))
           "improper shift amount")
   (riscv-type-i cgc rd rs1 (riscv-imm-int shamt) #x1000))
 
-(define (riscv-srli cgc rd rs1 shamt)
+(define (riscv-srli cgc rd rs1 shamt) ; XXX
   (assert (and (fx>= shamt 0) (fx< shamt (riscv-word-width cgc)))
           "improper shift amount")
   (riscv-type-i cgc rd rs1 (riscv-imm-int shamt) #x5000))
 
-(define (riscv-srai cgc rd rs1 shamt)
+(define (riscv-srai cgc rd rs1 shamt) ; XXX
   (assert (and (fx>= shamt 0) (fx< shamt (riscv-word-width cgc)))
           "improper shift amount")
   (riscv-type-i cgc rd rs1 (riscv-imm-int (fx+ #x400 shamt)) #x5000))
@@ -337,19 +418,19 @@
   (riscv-assert-64bit-mode cgc)
   (riscv-type-i cgc rd rs1 imm #x0000 #x1b))
 
-(define (riscv-slliw cgc rd rs1 shamt)
+(define (riscv-slliw cgc rd rs1 shamt) ; XXX
   (riscv-assert-64bit-mode cgc)
   (assert (and (fx>= shamt 0) (fx< shamt 32))
           "improper shift amount")
   (riscv-type-i cgc rd rs1 (riscv-imm-int shamt) #x1000 #x1b))
 
-(define (riscv-srliw cgc rd rs1 shamt)
+(define (riscv-srliw cgc rd rs1 shamt) ; XXX
   (riscv-assert-64bit-mode cgc)
   (assert (and (fx>= shamt 0) (fx< shamt 32))
           "improper shift amount")
   (riscv-type-i cgc rd rs1 (riscv-imm-int shamt) #x5000 #x1b))
 
-(define (riscv-sraiw cgc rd rs1 shamt)
+(define (riscv-sraiw cgc rd rs1 shamt) ; XXX
   (riscv-assert-64bit-mode cgc)
   (assert (and (fx>= shamt 0) (fx< shamt 32))
           "improper shift amount")
@@ -477,6 +558,18 @@
     (fx- (asm-label-pos (riscv-imm-lbl-label imm))
          (fx+ self (riscv-imm-lbl-offset imm))))
 
+  (define (instr-code imm)
+    (asm-32-le cgc
+               (fx+ opcode
+                    funct3
+                    (fxarithmetic-shift
+                      (riscv-reg-field rs1)
+                      15)
+                    (fxarithmetic-shift
+                      (riscv-reg-field rs2)
+                      20)
+                    (riscv-imm->instr imm))))
+
   (assert (and (riscv-reg? rs1)
                (riscv-reg? rs2)
                (riscv-imm? imm))
@@ -487,7 +580,8 @@
                    (eq? (riscv-imm-int-type imm) 'B)))
           "incorrect immediate type")
 
-  (if (riscv-imm-lbl? imm)
+  (if (riscv-imm-int? imm)
+      (instr-code imm)
       (asm-at-assembly cgc
                        (lambda (cgc self)
                          4)
@@ -495,30 +589,10 @@
                          (let* ((dist (label-dist self imm))
                                 (imm (riscv-imm-int dist 'B))) ; XXX
 
-                           (assert (and (fx>= dist -4096)
-                                        (fx<= dist 4095))
+                           (assert (and (fx>= dist -4096) (fx<= dist 4094))
                                    "branch label too far")
 
-                           (asm-32-le cgc
-                                      (fx+ opcode
-                                           funct3
-                                           (fxarithmetic-shift
-                                             (riscv-reg-field rs1)
-                                             15)
-                                           (fxarithmetic-shift
-                                             (riscv-reg-field rs2)
-                                             20)
-                                           (riscv-imm->instr imm))))))
-      (asm-32-le cgc
-                 (fx+ opcode
-                      funct3
-                      (fxarithmetic-shift
-                        (riscv-reg-field rs1)
-                        15)
-                      (fxarithmetic-shift
-                        (riscv-reg-field rs2)
-                        20)
-                      (riscv-imm->instr imm))))
+                           (instr-code imm)))))
 
   (if (codegen-context-listing-format cgc)
       (riscv-listing cgc
@@ -576,6 +650,14 @@
     (fx- (asm-label-pos (riscv-imm-lbl-label imm))
          (fx+ self (riscv-imm-lbl-offset imm))))
 
+  (define (instr-code imm)
+    (asm-32-le cgc
+               (fx+ opcode
+                    (fxarithmetic-shift
+                      (riscv-reg-field rd)
+                      7)
+                    (riscv-imm->instr imm))))
+
   (assert (and (riscv-reg? rd)
                (riscv-imm? imm))
           "invalid operands")
@@ -585,7 +667,8 @@
                    (eq? (riscv-imm-int-type imm) 'J)))
           "incorrect immediate type")
 
-  (if (riscv-imm-lbl? imm)
+  (if (riscv-imm-int? imm)
+      (instr-code imm)
       (asm-at-assembly cgc
                        (lambda (cgc self)
                          4)
@@ -593,22 +676,10 @@
                          (let* ((dist (label-dist self imm))
                                 (imm (riscv-imm-int dist 'J))) ; XXX
 
-                           (assert (and (fx>= dist -1048576)
-                                        (fx<= dist 1048575))
+                           (assert (and (fx>= dist -1048576) (fx<= dist 1048574))
                                    "branch label too far")
 
-                           (asm-32-le cgc
-                                      (fx+ opcode
-                                           (fxarithmetic-shift
-                                             (riscv-reg-field rd)
-                                             7)
-                                           (riscv-imm->instr imm))))))
-      (asm-32-le cgc
-                 (fx+ opcode
-                      (fxarithmetic-shift
-                        (riscv-reg-field rd)
-                        7)
-                      (riscv-imm->instr imm)))))
+                           (instr-code imm))))))
 
 ;;;----------------------------------------------------------------------------
 
