b37bfade23c99af5c6a377713bdd4bb975757912
Marc Feeley <feeley@iro.umontreal.ca>
2019-07-15 07:24:13 -0400

    Improve testing of check-=

--- lib/gambit/test/test-test.scm
+++ lib/gambit/test/test-test.scm
@@ -2,7 +2,7 @@
 
 ;;; File: "gambit/test/test-test.scm"
 
-;;; Copyright (c) 2013-2018 by Marc Feeley, All Rights Reserved.
+;;; Copyright (c) 2013-2019 by Marc Feeley, All Rights Reserved.
 
 ;;;============================================================================
 
@@ -45,25 +45,27 @@
               "\"test-test.scm\"@44.39: FAILED (check-not-eq? 'a 'a) GOT a\n")
 
 (check-= 1.5 1.5)
-(check-= 1.5 1.5 0.001)
-(check-= 1.5 1.5 0.001 "testing that 1.5 is equal to 1.5")
+(check-= 1.4995 1.5 0.001)
+(check-= 1.4995 1.5 0.001 "testing that 1.5 is equal to 1.5")
 (check-equal? (failure-msg (lambda () (check-= 1.5 2.5)))
               "\"test-test.scm\"@50.39: FAILED (check-= 1.5 2.5) GOT 1.5\n")
+(check-equal? (failure-msg (lambda () (check-= 1.49 1.5 0.001)))
+              "\"test-test.scm\"@52.39: FAILED (check-= 1.49 1.5 .001) GOT 1.49\n")
 
 (check-true #t)
 (check-true #t "testing that #t is true")
 (check-equal? (failure-msg (lambda () (check-true 123)))
-              "\"test-test.scm\"@55.39: FAILED (check-true 123) GOT 123\n")
+              "\"test-test.scm\"@57.39: FAILED (check-true 123) GOT 123\n")
 
 (check-false #f)
 (check-false #f "testing that #f is false")
 (check-equal? (failure-msg (lambda () (check-false 123)))
-              "\"test-test.scm\"@60.39: FAILED (check-false 123) GOT 123\n")
+              "\"test-test.scm\"@62.39: FAILED (check-false 123) GOT 123\n")
 
 (check-not-false 123)
 (check-not-false 123 "testing that 123 is not false")
 (check-equal? (failure-msg (lambda () (check-not-false #f)))
-              "\"test-test.scm\"@65.39: FAILED (check-not-false #f) GOT #f\n")
+              "\"test-test.scm\"@67.39: FAILED (check-not-false #f) GOT #f\n")
 
 (check-exn divide-by-zero-exception?
            (lambda () (/ 1 0)))
@@ -72,10 +74,10 @@
            "testing that (/ 1 0) raises a divide-by-zero exception")
 (check-equal? (failure-msg (lambda () (check-exn pair?
                                                  (lambda () (/ 1 0)))))
-              "\"test-test.scm\"@73.39: FAILED (check-exn pair? (lambda () (/ 1 0))) GOT #<divide-by-zero-exception #2>\n")
+              "\"test-test.scm\"@75.39: FAILED (check-exn pair? (lambda () (/ 1 0))) GOT #<divide-by-zero-exception #2>\n")
 (check-equal? (failure-msg (lambda () (check-exn divide-by-zero-exception?
                                                  (lambda () (/ 1 2)))))
-              "\"test-test.scm\"@76.39: FAILED (check-exn divide-by-zero-exception? (lambda () (/ 1 2))) GOT 1/2\n")
+              "\"test-test.scm\"@78.39: FAILED (check-exn divide-by-zero-exception? (lambda () (/ 1 2))) GOT 1/2\n")
 
 (check-tail-exn divide-by-zero-exception?
                 (lambda () (/ 1 0)))
@@ -84,9 +86,9 @@
                 "testing that (/ 1 0) raises a divide-by-zero exception in tail position")
 (check-equal? (failure-msg (lambda () (check-tail-exn pair?
                                                       (lambda () (/ 1 0)))))
-              "\"test-test.scm\"@85.39: FAILED (check-tail-exn pair? (lambda () (/ 1 0))) GOT #<divide-by-zero-exception #3>\n")
+              "\"test-test.scm\"@87.39: FAILED (check-tail-exn pair? (lambda () (/ 1 0))) GOT #<divide-by-zero-exception #3>\n")
 (check-equal? (failure-msg (lambda () (check-tail-exn divide-by-zero-exception?
                                                       (lambda () (/ 1 2)))))
-              "\"test-test.scm\"@88.39: FAILED (check-tail-exn divide-by-zero-exception? (lambda () (/ 1 2))) GOT 1/2\n")
+              "\"test-test.scm\"@90.39: FAILED (check-tail-exn divide-by-zero-exception? (lambda () (/ 1 2))) GOT 1/2\n")
 
 ;;;============================================================================
