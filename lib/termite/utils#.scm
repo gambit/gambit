38c028c37e2b56208501e4f6a18f3949b1dfd831
Frederic Hamel <frederic.hamel123@gmail.com>
2019-09-19 14:00:55 -0400

    Add termite/utils missing files

---
+++ lib/termite/utils#.scm
@@ -0,0 +1,12 @@
+;; utils
+
+;; ----------------------------------------------------------------------------
+;; Some basic utilities
+
+(##namespace ("termite/utils#"
+              filter
+              remove
+              quoted-symbol?
+              unquoted-symbol?
+
+              make-uuid))
