509d906265934db309f85fb0bd1a6dd7015c2322
Marc Feeley <feeley@iro.umontreal.ca>
2019-11-04 20:44:24 -0500

    Prevent infinite loop when trying to import _define-library in order to handle (import foo)

--- lib/_module.scm
+++ lib/_module.scm
@@ -653,21 +653,22 @@
              modstr
              " is required but is not installed.\nDownload and install (y/n)? ")))))
 
-  (if ##debug-modules? (pp (list '##install-module modref)));;;;;;;;;;;;;;;;;
+  (and (pair? (macro-modref-host modref)) ;; only install hosted modules
+       (begin
+         (if ##debug-modules? (pp (list '##install-module modref)));;;;;;;;;;;;;;;;;
+         (let ((mod-string (##modref->string modref)))
 
-  (let ((mod-string (##modref->string modref)))
-    (and (or (not (pair? (macro-modref-host modref)))
-             (##member
-              mod-string
-              (##os-module-whitelist)
-              (lambda (a b)
-                (module-prefix=? a b)))
-             ;; Ask user to install.
-             (module-install-confirm? mod-string))
+           (and (or (##member
+                     mod-string
+                     (##os-module-whitelist)
+                     (lambda (a b)
+                       (module-prefix=? a b)))
+                    ;; Ask user to install.
+                    (module-install-confirm? mod-string))
 
-         ((##eval '(let () (##import _pkg) install)) mod-string)
-         ;; Return the modref
-         modref)))
+                ((##eval '(let () (##import _pkg) install)) mod-string)
+                ;; Return the modref
+                modref)))))
 
 (define-prim (##install-module-set! x)
   (set! ##install-module x))
