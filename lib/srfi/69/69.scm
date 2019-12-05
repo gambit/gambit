e6bc43837f408bbadb20b64bfdff3980fb8ab998
Tchido-o <antoinedoucet18@gmail.com>
2019-12-05 10:20:52 -0500

    Add test and type check (srfi 69)

--- lib/srfi/69/69.scm
+++ lib/srfi/69/69.scm
@@ -23,6 +23,31 @@
 (declare (block))             ;; claim no global is assigned
 
 ;;;============================================================================
+;;; Local version of an unexported macro definition
+
+(define-fail-check-type table (macro-type-table))
+(define-check-type table (macro-type-table)
+  macro-table?)
+
+;;;============================================================================
+
+(define-syntax with-table-check
+    (syntax-rules ()
+        ((with-table-check (function-name hash-table . rest) function-def)
+         (macro-force-vars (hash-table . rest)
+          (macro-check-table
+            hash-table
+            1
+            (function-name hash-table . rest)
+            function-def)))))
+
+(define-syntax define-table-check
+    (syntax-rules ()
+        ((define-table-check (function-name hash-table . rest) function-def)
+         (define (function-name hash-table . rest)
+            (with-table-check (function-name hash-table . rest) function-def)))))
+
+;;;============================================================================
 
 ;;; Type constructors and predicate
 
@@ -33,32 +58,74 @@
          #!optional
          (equal-fn (macro-absent-obj))
          (hash-fn (macro-absent-obj)))
-  (if (eq? equal-fn (macro-absent-obj))
-      (make-table)
-      (if (eq? hash-fn (macro-absent-obj))
-          (make-table test: equal-fn)
-          (make-table test: equal-fn hash: hash-fn))))
-
+     (if (eq? equal-fn (macro-absent-obj))
+          (make-table)
+          (macro-force-vars (equal-fn)
+            (macro-check-procedure
+              equal-fn
+              2
+              (make-hash-table equal-fn hash-fn)
+              (if (eq? hash-fn (macro-absent-obj))
+                  (make-table test: equal-fn)
+                  (macro-force-vars (hash-fn)
+                    (macro-check-procedure
+                       hash-fn
+                       3
+                       (make-hash-table equal-fn hash-fn)
+                       (make-table test: equal-fn hash: hash-fn))))))))
+ 
 (define (alist->hash-table
          alist
          #!optional
          (equal-fn (macro-absent-obj))
          (hash-fn (macro-absent-obj)))
-  (if (eq? equal-fn (macro-absent-obj))
-      (list->table alist)
-      (if (eq? hash-fn (macro-absent-obj))
-          (list->table alist test: equal-fn)
-          (list->table alist test: equal-fn hash: hash-fn))))
+     (let ((ht
+       (if (eq? equal-fn (macro-absent-obj))
+          (make-table)
+          (macro-force-vars (equal-fn)
+            (macro-check-procedure
+              equal-fn
+              2
+              (alist->hash-table alist equal-fn hash-fn)
+              (if (eq? hash-fn (macro-absent-obj))
+                  (make-table test: equal-fn)
+                  (macro-force-vars (hash-fn)
+                    (macro-check-procedure
+                       hash-fn
+                       3
+                       (alist->hash-table alist equal-fn hash-fn)
+                       (make-table test: equal-fn hash: hash-fn)))))))))
+
+    (let loop ((x alist))
+      (macro-force-vars (x)
+        (if (pair? x)
+          (let ((couple (car x)))
+            (macro-force-vars (couple)
+              (macro-check-pair-list
+               couple
+               1
+               (alist->hash-table alist equal-fn hash-fn)
+               (let ((key (car couple)))
+                 (if (eq? ht (table-ref ht key ht))
+                   (table-set! ht key (cdr couple)))
+                 (loop (cdr x))))))
+          (macro-check-list
+           x
+           1
+           (alist->hash-table alist equal-fn hash-fn)
+           ht))))))
+
+
 
 ;;;============================================================================
 
 ;;; Reflexion
 
-(define (hash-table-equivalence-function ht)
+(define-table-check (hash-table-equivalence-function ht)
   (or (macro-table-test ht)
       eq?))
 
-(define (hash-table-hash-function ht)
+(define-table-check (hash-table-hash-function ht)
   (or (macro-table-hash ht)
       eq?-hash))
 
@@ -67,71 +134,105 @@
 ;;; Dealing with single elements
 
 (define (hash-table-ref ht key #!optional (thunk (macro-absent-obj)))
-  (let ((val (table-ref ht key (macro-deleted-obj))))
-    (if (eq? val (macro-deleted-obj))
-        (if (eq? thunk (macro-absent-obj))
-            (##raise-unbound-key-exception hash-table-ref ht key thunk)
-            (thunk))
-        val)))
+        (with-table-check (hash-table-ref ht key thunk)
+            (if (eq? thunk (macro-absent-obj))
+                (table-ref ht key)
+                  (macro-check-procedure
+                    thunk
+                    3
+                    (hash-table-ref ht key thunk)
+                    (let ((x (table-ref ht key (macro-deleted-obj))))
+                                  (if (eq? x (macro-deleted-obj))
+                                      (thunk)
+                                      x))))))
 
-(define (hash-table-ref/default ht key default)
+(define-table-check (hash-table-ref/default ht key default)
   (table-ref ht key default))
 
-(define (hash-table-set! ht key val)
+(define-table-check (hash-table-set! ht key val)
   (table-set! ht key val))
 
-(define (hash-table-delete! ht key)
+(define-table-check (hash-table-delete! ht key)
   (table-set! ht key))
 
-(define (hash-table-exists? ht key)
+(define-table-check (hash-table-exists? ht key)
   (not (eq? (table-ref ht key (macro-deleted-obj))
             (macro-deleted-obj))))
+
 
 (define (hash-table-update! ht key func #!optional (thunk (macro-absent-obj)))
 
   (define (update! val)
     (table-set! ht key (func val)))
+  
+  (with-table-check (hash-table-update! ht key func thunk)
+      (let ((val (table-ref ht key (macro-deleted-obj))))
+           (macro-check-procedure 
+           func
+           3
+           (hash-table-update! ht key func thunk)     
+           (if (eq? val (macro-deleted-obj))
+               (if (eq? thunk (macro-absent-obj))
+                   (##raise-unbound-key-exception 
+                     hash-table-update! ht key func thunk)
+                   (macro-check-procedure
+                     thunk
+                     4
+                     (hash-table-update! ht key func thunk)
+                     (update! (thunk))))
+               (update! val))))))
 
-  (let ((val (table-ref ht key (macro-deleted-obj))))
-    (if (eq? val (macro-deleted-obj))
-        (if (eq? thunk (macro-absent-obj))
-            (##raise-unbound-key-exception hash-table-update! ht key func thunk)
-            (update! (thunk)))
-        (update! val))))
 
-(define (hash-table-update!/default ht key func default)
-  (table-set! ht key (func (table-ref ht key default))))
+(define-table-check (hash-table-update!/default ht key func default)
+  (macro-check-procedure
+    func
+    3
+    (hash-table-update!/default ht key func default)
+    (table-set! ht key (func (table-ref ht key default)))))
 
 ;;;============================================================================
 
 ;;; Dealing with the whole contents
 
-(define (hash-table-size ht)
+(define-table-check (hash-table-size ht)
   (table-length ht))
 
-(define (hash-table-keys ht)
+(define-table-check (hash-table-keys ht)
   (map car (table->list ht)))
 
-(define (hash-table-values ht)
+(define-table-check (hash-table-values ht)
   (map cdr (table->list ht)))
 
-(define (hash-table-walk ht proc)
-  (table-for-each proc ht))
+(define-table-check (hash-table-walk ht proc)
+  (macro-check-procedure
+    proc
+    2
+    (hash-table-walk ht proc)
+    (table-for-each proc ht)))
 
-(define (hash-table-fold ht f init-value)
-  (fold (lambda (elem prec)
-          (f (car elem) (cdr elem) prec))
-        init-value
-        (table->list ht)))
+(define-table-check (hash-table-fold ht func init-value)
+  (macro-check-procedure 
+    func
+    2
+    (hash-table-fold ht func init-value)
+    (fold (lambda (elem prec)
+            (func (car elem) (cdr elem) prec))
+          init-value
+          (table->list ht))))
 
-(define (hash-table->alist ht)
+(define-table-check (hash-table->alist ht)
   (table->list ht))
 
-(define (hash-table-copy ht)
+(define-table-check (hash-table-copy ht)
   (table-copy ht))
 
 (define (hash-table-merge! ht1 ht2)
-  (table-merge! ht1 ht2 #f))
+  (with-table-check (hash-table-merge! ht1 ht2)
+    (macro-check-table 
+      ht2
+      2
+      (hash-table-merge! ht1 ht2)
+      (table-merge! ht1 ht2 #f))))
 
 (define (hash obj #!optional (bound (macro-max-fixnum32)))
   (modulo (equal?-hash obj) bound))
--- lib/srfi/69/test.scm
+++ lib/srfi/69/test.scm
@@ -16,9 +16,10 @@
 
 ;;; Type constructors and predicate
 
-;;; make-hash-table and hash-table?
 
-(check-false (hash-table? 1))
+;;; hash-table?
+
+(check-false (hash-table? 0))
 
 (check-true (hash-table? (make-hash-table)))
 
@@ -28,12 +29,36 @@
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table? 1 2)))
+ (lambda () (hash-table? 0 1)))
+
+
+;;; make-hash-table
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (make-hash-table (lambda () '()) (lambda () '()) 0)))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (make-hash-table 0 (lambda () '()))))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (make-hash-table (lambda () '()) 0)))
 
 
 ;;; alist->hash-table
 
+(check-true (hash-table? (alist->hash-table '())))
 (check-true (hash-table? (alist->hash-table '((0 . 0) (1 . 1) (2 . 2)))))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (alist->hash-table 0)))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (alist->hash-table '(0))))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
@@ -41,15 +66,29 @@
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (alist->hash-table '() 0 0 0)))
+ (lambda () (alist->hash-table '() 1 2 3)))
 
 
 ;;;============================================================================
 
 ;;; Reflexion
 
+
+;; hash-table-equivalence-function
+
 (check-equal? (hash-table-equivalence-function (make-hash-table))
               ##equal?)
+
+(let ((proc (lambda () '())))
+    (check-equal? (hash-table-equivalence-function (make-hash-table proc))
+                  proc)
+
+    (check-equal? (hash-table-equivalence-function (alist->hash-table '() proc))
+                  proc))
+
+(check-tail-exn
+  type-exception?
+  (lambda () (hash-table-equivalence-function 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
@@ -60,8 +99,28 @@
  (lambda () (hash-table-equivalence-function (make-hash-table) 0)))
 
 
+
+;;;----------------------------------------------------------------------------
+
+;; hash-table-hash-function
+
 (check-equal? (hash-table-hash-function (make-hash-table))
               ##equal?-hash)
+
+(let ((proc1 (lambda () '()))
+      (proc2 (lambda () '())))
+    
+    (check-not-equal? proc1 proc2)
+
+    (check-equal? (hash-table-hash-function (make-hash-table proc1 proc2))
+              proc2)
+
+    (check-equal? (hash-table-hash-function (alist->hash-table '() proc1 proc2))
+              proc2))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (hash-table-hash-function 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
@@ -69,15 +128,14 @@
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-hash-function (make-hash-table) 0)))
-
+ (lambda () (hash-table-hash-function (make-hash-table) 1)))
 
 ;;;============================================================================
 
 ;;; Dealing with single elements
 
-;;; hash-table-ref
 
+;;; hash-table-ref
 (check-equal?
  (hash-table-ref (alist->hash-table '((0 . 0) (1 . 1))) 0)
  0)
@@ -98,13 +156,24 @@
  (lambda () (hash-table-ref (make-hash-table) 0)))
 
 (check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-ref 0 1)))
+
+(check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-ref (make-hash-table) 1 2)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-ref (make-hash-table))))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-ref (make-hash-table) 0 0 0)))
+ (lambda () (hash-table-ref (make-hash-table) 1 2 3)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-ref/default
 
@@ -117,13 +186,19 @@
  #f)
 
 (check-tail-exn
- wrong-number-of-arguments-exception?
- (lambda () (hash-table-ref/default (make-hash-table) 0)))
+ type-exception?
+ (lambda ()
+  (hash-table-ref/default 0 0 #f)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-ref/default (make-hash-table) 0 0 0)))
+ (lambda () (hash-table-ref/default (make-hash-table) 1)))
 
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash-table-ref/default (make-hash-table) 1 2 3)))
+
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-set!
 
@@ -132,13 +207,19 @@
   (check-equal? (hash-table-ref ht 0) 1))
 
 (check-tail-exn
- wrong-number-of-arguments-exception?
- (lambda () (hash-table-set! (make-hash-table) 0)))
+ type-exception?
+ (lambda ()
+  (hash-table-set! 0 0 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-set! (make-hash-table) 0 1 1)))
+ (lambda () (hash-table-set! (make-hash-table) 1)))
 
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash-table-set! (make-hash-table) 1 2 3)))
+
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-delete!
 
@@ -148,13 +229,19 @@
   (check-equal? (hash-table-ref ht 0 (lambda () #f)) #f))
 
 (check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-delete! 0 0)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-delete! (make-hash-table))))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-delete! (make-hash-table) 0 0)))
+ (lambda () (hash-table-delete! (make-hash-table) 1 2)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-exists?
 
@@ -164,13 +251,19 @@
   (check-false (hash-table-exists? ht 0)))
 
 (check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-exists? 0 0)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-exists? (make-hash-table))))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-exists? (make-hash-table) 0 0)))
+ (lambda () (hash-table-exists? (make-hash-table) 1 2)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-update!
 
@@ -186,15 +279,31 @@
              (lambda (val) (+ val 1)))))
 
 (check-tail-exn
- wrong-number-of-arguments-exception?
+ type-exception?
  (lambda ()
-   (hash-table-update! (make-hash-table) 0)))
+  (hash-table-update! 0 0 (lambda () '()))))
+
+(check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-update! (make-hash-table) 0 0)))
+
+(check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-update! (make-hash-table) 0 (lambda () '()) 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda ()
-   (hash-table-update! (make-hash-table) 0 (lambda () '()) 0 0)))
+   (hash-table-update! (make-hash-table) 1)))
 
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda ()
+   (hash-table-update! (make-hash-table) 1 (lambda () 2) (lambda () 3) 4)))
+
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-update!/default
 
@@ -203,19 +312,30 @@
   (check-equal? (hash-table-ref ht 0) 1))
 
 (check-tail-exn
- wrong-number-of-arguments-exception?
+ type-exception?
  (lambda ()
-   (hash-table-update!/default (make-hash-table) 0 (lambda () '()))))
+  (hash-table-update!/default 0 0 (lambda () '()) 0)))
+
+(check-tail-exn
+ type-exception?
+ (lambda ()
+  (hash-table-update!/default (make-hash-table) 0 0 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda ()
-   (hash-table-update!/default (make-hash-table) 0 (lambda () '()) 0 0)))
+   (hash-table-update!/default (make-hash-table) 1 (lambda () 2))))
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda ()
+   (hash-table-update!/default (make-hash-table) 1 (lambda () 2) 3 4)))
 
 
 ;;;============================================================================
 
 ;;; dealing with the whole contents
+
 
 ;;; hash-table-size
 
@@ -224,36 +344,49 @@
 (check-equal? (hash-table-size (alist->hash-table '((0 . 0) (1 . 1)))) 2)
 
 (check-tail-exn
+ type-exception?
+ (lambda () (hash-table-size 0)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-size)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-size (make-hash-table) 0)))
+ (lambda () (hash-table-size (make-hash-table) 1)))
 
+;;;----------------------------------------------------------------------------
 
-;;; hash-table-keys and hash-table-values
-
+;;; hash-table-keys
 (let ((ht (alist->hash-table '((0 . 2) (1 . 3)))))
   (check-true (fold
-               (lambda (elem res)
-                 (if (equal? res #t)
-                     #t
-                     (table-search (lambda (key val)
-                                     (if (equal? key elem)
-                                         #t
-                                         #f)) ht)))
-               #f
-               (hash-table-keys ht))))
+                (lambda (elem res)
+                  (if (equal? res #t)
+                      #t
+                      (table-search 
+                        (lambda (key val)
+                          (if (equal? key elem)
+                              #t
+                              #f)) 
+                        ht)))
+                #f
+                (hash-table-keys ht))))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (hash-table-keys 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-keys (make-hash-table) 0)))
+ (lambda () (hash-table-keys (make-hash-table) 1)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-keys)))
 
+;;;----------------------------------------------------------------------------
+
+;;; hash-table-values
 
 (let ((ht (alist->hash-table '((0 . 2) (1 . 3)))))
   (check-true
@@ -265,13 +398,18 @@
          (hash-table-values ht))))
 
 (check-tail-exn
+ type-exception?
+ (lambda () (hash-table-values 0)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-values (make-hash-table) 0)))
+ (lambda () (hash-table-values (make-hash-table) 1)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-values)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-walk
 
@@ -280,13 +418,22 @@
   (check-equal? (hash-table-ref ht 0) 0))
 
 (check-tail-exn
+ type-exception?
+ (lambda () (hash-table-walk 0 (lambda () 1))))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (hash-table-walk (make-hash-table) 1)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-walk (make-hash-table))))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-walk (make-hash-table) (lambda () '()) 0)))
+ (lambda () (hash-table-walk (make-hash-table) (lambda () 1) 2)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-fold
 
@@ -300,13 +447,22 @@
   (check-equal? (hash-table-ref ht 2) 0))
 
 (check-tail-exn
- wrong-number-of-arguments-exception?
- (lambda () (hash-table-fold (make-hash-table) (lambda () '()))))
+ type-exception?
+ (lambda () (hash-table-fold 0 (lambda () '()) 0)))
+
+(check-tail-exn
+ type-exception?
+ (lambda () (hash-table-fold (make-hash-table) 0 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table-fold (make-hash-table) (lambda () '()) 0 0)))
+ (lambda () (hash-table-fold (make-hash-table) (lambda () 1))))
 
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash-table-fold (make-hash-table) (lambda () 1) 2 3)))
+
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table->alist
 
@@ -320,6 +476,9 @@
                                 lst)))
                     #t
                     (hash-table->alist (alist->hash-table lst)))))
+(check-tail-exn
+ type-exception?
+ (lambda () (hash-table->alist 0)))
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
@@ -327,8 +486,9 @@
 
 (check-tail-exn
  wrong-number-of-arguments-exception?
- (lambda () (hash-table->alist (make-hash-table) 0)))
+ (lambda () (hash-table->alist (make-hash-table) 1)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-copy
 
@@ -339,6 +499,10 @@
   (check-not-equal? ht1 ht2))
 
 (check-tail-exn
+ type-exception?
+ (lambda() (hash-table-copy 0)))
+
+(check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-copy)))
 
@@ -346,6 +510,7 @@
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-copy (make-hash-table) 0)))
 
+;;;----------------------------------------------------------------------------
 
 ;;; hash-table-merge!
 
@@ -354,6 +519,15 @@
       (ht3 (alist->hash-table '((0 . 0) (1 . 1) (2 . 2) (3 . 3)))))
   (check-equal? ht3 (hash-table-merge! ht1 ht2)))
 
+
+(check-tail-exn
+ type-exception?
+ (lambda() (hash-table-merge! 0 (make-hash-table))))
+
+(check-tail-exn
+ type-exception?
+ (lambda() (hash-table-merge! (make-hash-table) 1)))
+
 (check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-merge! (make-hash-table))))
@@ -361,5 +535,53 @@
 (check-tail-exn
  wrong-number-of-arguments-exception?
  (lambda () (hash-table-merge! (make-hash-table) (make-hash-table) 0)))
+
+;;;----------------------------------------------------------------------------
+
+;;; hash
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash)))
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash 0 1 2)))
+
+;;;----------------------------------------------------------------------------
+
+;;; string-hash
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (string-hash)))
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (string-hash "0" 1 2)))
+
+;;;----------------------------------------------------------------------------
+
+;;; string-ci-hash
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (string-ci-hash)))
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (string-ci-hash "0" 1 2)))
+
+;;;----------------------------------------------------------------------------
+
+;;; hash-by-identity
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash-by-identity)))
+
+(check-tail-exn
+ wrong-number-of-arguments-exception?
+ (lambda () (hash-by-identity 0 1 2)))
 
 ;;;============================================================================
