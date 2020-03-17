b018a7e99a1fff4fe8fa94f70249cac7162b37fe
Marc Feeley <feeley@iro.umontreal.ca>
2020-03-17 10:21:38 -0400

    Move implementation of parameter objects to respect initialization order

--- lib/_thread.scm
+++ lib/_thread.scm
@@ -220,45 +220,6 @@
 
 ;;;----------------------------------------------------------------------------
 
-;;; Implementation of dynamic environments.
-
-(define ##parameter-counter 0)
-
-(define-prim (##make-parameter
-              init
-              #!optional
-              (f (macro-absent-obj)))
-  (let ((filter
-         (if (##eq? f (macro-absent-obj))
-           (lambda (x) x)
-           f)))
-    (macro-check-procedure filter 2 (make-parameter init f)
-      (let* ((val
-              (filter init))
-             (new-count
-              (##fxwrap+ ##parameter-counter 1)))
-        ;; Note: it is unimportant if the increment of
-        ;; ##parameter-counter is not atomic; it simply means a
-        ;; possible close repetition of the same hash code
-        (set! ##parameter-counter new-count)
-        (let ((descr
-               (macro-make-parameter-descr
-                val
-                (##partial-bit-reverse new-count)
-                filter)))
-          (letrec ((param
-                    (lambda (#!optional (new-val (macro-absent-obj)))
-                      (if (##eq? new-val (macro-absent-obj))
-                        (##dynamic-ref param)
-                        (##dynamic-set!
-                         param
-                         ((macro-parameter-descr-filter descr) new-val))))))
-            param))))))
-
-(define-prim (make-parameter init #!optional (f (macro-absent-obj)))
-  (macro-force-vars (f)
-    (##make-parameter init f)))
-
 (define ##current-exception-handler
   (##make-parameter
    (lambda (exc)
@@ -5514,45 +5475,6 @@
   `(##void));;;;;;;;;;;*************
 
 ;;;----------------------------------------------------------------------------
-
-;;; Implementation of dynamic environments.
-
-(define ##parameter-counter 0)
-
-(define-prim (##make-parameter
-              init
-              #!optional
-              (f (macro-absent-obj)))
-  (let ((filter
-         (if (##eq? f (macro-absent-obj))
-           (lambda (x) x)
-           f)))
-    (macro-check-procedure filter 2 (make-parameter init f)
-      (let* ((val
-              (filter init))
-             (new-count
-              (##fx+ ##parameter-counter 1)))
-        ;; Note: it is unimportant if the increment of
-        ;; ##parameter-counter is not atomic; it simply means a
-        ;; possible close repetition of the same hash code
-        (set! ##parameter-counter new-count)
-        (let ((descr
-               (macro-make-parameter-descr
-                val
-                (##partial-bit-reverse new-count)
-                filter)))
-          (letrec ((param
-                    (lambda (#!optional (new-val (macro-absent-obj)))
-                      (if (##eq? new-val (macro-absent-obj))
-                        (##dynamic-ref param)
-                        (##dynamic-set!
-                         param
-                         ((macro-parameter-descr-filter descr) new-val))))))
-            param))))))
-
-(define-prim (make-parameter init #!optional (f (macro-absent-obj)))
-  (macro-force-vars (f)
-    (##make-parameter init f)))
 
 (define ##current-exception-handler
   (##make-parameter
