2846ec6567498e5b23abe419a480864877bcaf45
Frederic Hamel <frederic.hamel123@gmail.com>
2020-04-28 14:38:59 -0400

    fix unit-tests

--- lib/_uri/test.scm
+++ lib/_uri/test.scm
@@ -12,24 +12,24 @@
 (define query "a=1&b=2")
 (define query-result (string->uri-query query #f))
 
-(check-equal? query-result '(("a" . "1") ("b" . "2")))
+(test-equal '(("a" . "1") ("b" . "2")) query-result)
 
 (define uri (string->uri "example://example.com:2020" #f))
 
-(check-equal? (uri-scheme uri) "example")
-(check-true (uri-slashes uri))
-(check-equal? (uri-authority uri) "example.com:2020")
-(check-equal? (uri-path uri) "/")
-(check-false (uri-query uri))
-(check-false (uri-fragment uri))
+(test-equal "example" (uri-scheme uri))
+(test-eq #t (uri-slashes uri))
+(test-equal "example.com:2020" (uri-authority uri))
+(test-equal "/" (uri-path uri))
+(test-eq #f (uri-query uri))
+(test-eq #f (uri-fragment uri))
 
 (define uri-encoded (string->uri (encode-for-uri "example://example.com/Aéà") #t))
 
-(check-equal? (uri-scheme uri-encoded) "example")
-(check-true (uri-slashes uri-encoded))
-(check-equal? (uri-authority uri-encoded) "example.com")
-(check-equal? (uri-path uri-encoded) "/Aéà")
-(check-false (uri-query uri-encoded))
-(check-false (uri-fragment uri-encoded))
+(test-equal "example" (uri-scheme uri-encoded))
+(test-eq #t (uri-slashes uri-encoded))
+(test-equal "example.com" (uri-authority uri-encoded))
+(test-equal "/Aéà" (uri-path uri-encoded))
+(test-eq #f (uri-query uri-encoded))
+(test-eq #f (uri-fragment uri-encoded))
 
 ;;;============================================================================
