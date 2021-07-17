633f1c28d9ac274530f2d141e023860f96f982d2
Lassi Kortela <lassi@lassi.io>
2021-07-17 21:30:28 +0300

    Make exact-10^n-table an internal define

--- lib/gambit/unstable/unstable.scm
+++ lib/gambit/unstable/unstable.scm
@@ -13,25 +13,22 @@
 
 ;;;----------------------------------------------------------------------------
 
-;; TODO: The following are copied from lib/_num#.scm
+;; TODO: The following are adapted from lib/_num#.scm
 
 (##define-macro (macro-make-exact-10^n-table)
-
   (define max-exact-power-of-10 22)
   ;; (floor (inexact->exact (/ (log (expt 2 (macro-flonum-m-bits-plus-1)))
   ;;                           (log 5))))
-
   (let loop ((i max-exact-power-of-10) (t '()))
     (if (>= i 0)
         (loop (- i 1) (cons (exact->inexact (expt 10 i)) t))
         `',(list->f64vector t))))
 
-(define exact-10^n-table (macro-make-exact-10^n-table))
-
 (define-prim&proc (make-inexact-real (negative? boolean)
                                      (mantissa nonnegative-exact-integer)
                                      (exponent exact-integer 0)
                                      (precision object #f))
+  (define exact-10^n-table (macro-make-exact-10^n-table))
   (let ((n (if (and (fixnum? mantissa)
                     (primitive (fixnum->flonum-exact? mantissa))
                     (fixnum? exponent)
