a033a700af49d262d6869246b5e8673a1d92d7b9
Bradley Lucier <lucier@math.purdue.edu>
2022-10-12 21:12:24 -0400

    Update log tests.

--- tests/unit-tests/03-number/log.scm
+++ tests/unit-tests/03-number/log.scm
@@ -8,13 +8,13 @@
 
 ;;; Test branch cuts
 
-(check-eqv? (log -1)  +3.141592653589793i)
-(check-eqv? (log +i)  +1.5707963267948966i)
-(check-eqv? (log -i)  -1.5707963267948966i)
-(check-eqv? (log 0.+i)  0.+1.5707963267948966i)
-(check-eqv? (log 0.-i)  0.-1.5707963267948966i)
-(check-= (log -1+0.i) +3.141592653589793i)
-(check-= (log -1-0.i) -3.141592653589793i)
+(check-eqv? (log -1)  (make-rectangular 0 (macro-inexact-+pi)))
+(check-eqv? (log +i)  (make-rectangular 0 (macro-inexact-+pi/2)))
+(check-eqv? (log -i)  (make-rectangular 0 (macro-inexact--pi/2)))
+(check-eqv? (log 0.+i) (make-rectangular 0. (macro-inexact-+pi/2)))
+(check-eqv? (log 0.-i) (make-rectangular 0. (macro-inexact--pi/2)))
+(check-eqv? (log -1+0.i) (make-rectangular 0. (macro-inexact-+pi)))
+(check-eqv? (log -1-0.i) (make-rectangular 0. (macro-inexact--pi)))
 
 ;;; Test exceptions
 
@@ -32,6 +32,63 @@
 (check-eqv? (log 2 1/2) -1)
 (check-eqv? (log 1/2 2) -1)
 (check-eqv? (log 2 8) 1/3)
+
+;;; C99 tests
+
+;;; If z is -0+0i, the result is -∞+πi and FE_DIVBYZERO is raised
+(check-eqv? (log -0.+0.i) (make-rectangular -inf.0 (macro-inexact-+pi)))
+
+;;; If z is +0+0i, the result is -∞+0i and FE_DIVBYZERO is raised
+(check-eqv? (log +0.+0.i) -inf.0+0.i)
+
+;;; If z is x+∞i (for any finite x), the result is +∞+πi/2
+(check-eqv? (log 2.+inf.0i) (make-rectangular +inf.0 (macro-inexact-+pi/2)))
+
+;;; If z is x+NaNi (for any finite x), the result is NaN+NaNi and FE_INVALID may be raised
+(check-true (nan? (real-part (log 2.+nan.0i))))
+(check-true (nan? (imag-part (log 2.+nan.0i))))
+
+;;; If z is -∞+yi (for any finite positive y), the result is +∞+πi
+(check-eqv? (log -inf.0+2.i) (make-rectangular +inf.0 (macro-inexact-+pi)))
+
+;;; If z is +∞+yi (for any finite positive y), the result is +∞+0i
+(check-eqv? (log +inf.0+2.i) +inf.0+0.i)
+
+;;; If z is -∞+∞i, the result is +∞+3πi/4
+(check-eqv? (log -inf.0+inf.0i) (make-rectangular +inf.0 (macro-inexact-+3pi/4)))
+
+;;; If z is +∞+∞i, the result is +∞+πi/4
+(check-eqv? (log +inf.0+inf.0i) (make-rectangular +inf.0 (macro-inexact-+pi/4)))
+
+;;; If z is ±∞+NaNi, the result is +∞+NaNi
+(check-eqv? (real-part (log +inf.0+nan.0i)) +inf.0)
+(check-true (nan? (imag-part (log +inf.0+nan.0i))))
+
+(check-eqv? (real-part (log -inf.0+nan.0i)) +inf.0)
+(check-true (nan? (imag-part (log -inf.0+nan.0i))))
+
+;;; If z is NaN+yi (for any finite y), the result is NaN+NaNi and FE_INVALID may be raised
+(check-true (nan? (real-part (log +nan.0+2.i))))
+(check-true (nan? (imag-part (log +nan.0+2.i))))
+
+;;; If z is NaN+∞i, the result is +∞+NaNi
+(check-eqv? (real-part (log +nan.0+inf.0i)) +inf.0)
+(check-true (nan? (imag-part (log +nan.0+inf.0i))))
+
+;;; If z is NaN+NaNi, the result is NaN+NaNi
+(check-true (nan? (real-part (log +nan.0+nan.0i))))
+(check-true (nan? (imag-part (log +nan.0+nan.0i))))
+
+;;; std::log(std::conj(z)) == std::conj(std::log(z))
+
+(let ((args '(+0. -0. +2.0 -2.0 +inf.0 -inf.0)))
+  (for-each (lambda (x)
+              (for-each (lambda (y)
+                          (let ((z (make-rectangular x y)))
+                            (check-eqv? (log (conjugate z))
+                                        (conjugate (log z)))))
+                        args))
+            args))
 
 ;;; Test exceptions
 
