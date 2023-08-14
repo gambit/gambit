d56f866e1a3c07c7691f514013ad04d8c798e2a8
Mathis <ecomath360@gmail.com>
2023-08-14 15:16:54 -0400

    Added Ribbit and Ribbit-int as bench targets

---
+++ bench/prefix/prefix-ribbit-int.scm
@@ -0,0 +1,33 @@
+;INSERTCODE
+;------------------------------------------------------------------------------
+
+(define (time x) x) ;; noop until ribbit has a way of mesuring time
+
+(define (run-bench name count ok? run)
+  (letrec ((loop 
+             (lambda (i result)
+               (if (< 0 i)
+                 (loop (- i 1) (run))
+                 result))))
+    (loop count '(undefined))))
+
+(define (run-benchmark name count ok? run-maker . args)
+  (newline)
+  (let ((run (apply run-maker args)))
+    (let ((result (time (run-bench name count ok? run))))
+      (if (not (ok? result))
+        (begin
+          (display "*** wrong result ***")
+          (newline)
+          (display "*** got: ")
+          (write result)
+          (newline))))))
+
+(define (fatal-error . args)
+  (for-each display args)
+  (newline)
+  (##exit 1))
+
+ (define (call-with-output-file/truncate filename proc)
+   (call-with-output-file filename proc))
+
