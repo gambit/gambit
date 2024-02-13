b3df41a7ccf00c50fb50d0f552cc026b7e3716f0
Bradley Lucier <lucier@math.purdue.edu>
2024-02-13 15:42:28 -0500

    Small fixes.

--- tests/unit-tests/07-vector/vector_cumulate.scm
+++ tests/unit-tests/07-vector/vector_cumulate.scm
@@ -3,7 +3,7 @@
 (check-equal? '#(3 4 8 9 14 23 25 30 36)
               (vector-cumulate + 0 '#(3 1 4 1 5 9 2 5 6)))
 
-(check-tail-exn type-exception? (lambda () (vector-cumulate 4 4)))
+(check-tail-exn type-exception? (lambda () (vector-cumulate 4 4 'a)))
 (check-tail-exn type-exception? (lambda () (vector-cumulate values 'a 'a)))
 
 (check-tail-exn wrong-number-of-arguments-exception? (lambda () (vector-cumulate)))
