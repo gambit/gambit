32945384f52e158616481568bf6a9d398f791e86
Peter1169 <46818665+Peter1169@users.noreply.github.com>
2026-06-24 00:16:34 +0200

    Avoid Windows host-info lookup tests

--- tests/unit-tests/12-os/host_info.scm
+++ tests/unit-tests/12-os/host_info.scm
@@ -8,33 +8,37 @@
     (##char=? #\\ directory-separator)))
 
 
-(test-assert (host-info? (##host-info "localhost")))
 (if (not ##windows?)
-    (test-assert (host-info? (##host-info '#u8(127 0 0 1)))))
+    (begin
+      (test-assert (host-info? (##host-info "localhost")))
+      (test-assert (host-info? (##host-info '#u8(127 0 0 1))))))
 
-;;(test-assert (string? (##host-info-name (##host-info "localhost"))))
-;;(test-assert (list? (##host-info-aliases (##host-info "localhost"))))
-;;(test-assert (list? (##host-info-addresses (##host-info "localhost"))))
+(define localhost-info
+  (and (not ##windows?)
+       (host-info "localhost")))
 
-
-(test-assert (host-info? (host-info "localhost")))
 (if (not ##windows?)
-    (test-assert (host-info? (host-info '#u8(127 0 0 1)))))
+    (begin
+      (test-assert (host-info? localhost-info))
+      (test-assert (host-info? (host-info '#u8(127 0 0 1))))
 
-(test-assert (string? (host-info-name (host-info "localhost"))))
-(test-assert (list? (host-info-aliases (host-info "localhost"))))
-(test-assert (list? (host-info-addresses (host-info "localhost"))))
+      ;;(test-assert (string? (##host-info-name (##host-info "localhost"))))
+      ;;(test-assert (list? (##host-info-aliases (##host-info "localhost"))))
+      ;;(test-assert (list? (##host-info-addresses (##host-info "localhost"))))
 
+      (test-assert (string? (host-info-name localhost-info)))
+      (test-assert (list? (host-info-aliases localhost-info)))
+      (test-assert (list? (host-info-addresses localhost-info)))))
 
 (test-error-tail wrong-number-of-arguments-exception? (host-info))
-(test-error-tail wrong-number-of-arguments-exception? (host-info "localhost" #f))
+(test-error-tail wrong-number-of-arguments-exception? (host-info #f #f))
 
 (test-error-tail wrong-number-of-arguments-exception? (host-info-name))
-(test-error-tail wrong-number-of-arguments-exception? (host-info-name (host-info "localhost") #f))
+(test-error-tail wrong-number-of-arguments-exception? (host-info-name #f #f))
 (test-error-tail wrong-number-of-arguments-exception? (host-info-aliases))
-(test-error-tail wrong-number-of-arguments-exception? (host-info-aliases (host-info "localhost") #f))
+(test-error-tail wrong-number-of-arguments-exception? (host-info-aliases #f #f))
 (test-error-tail wrong-number-of-arguments-exception? (host-info-addresses))
-(test-error-tail wrong-number-of-arguments-exception? (host-info-addresses (host-info "localhost") #f))
+(test-error-tail wrong-number-of-arguments-exception? (host-info-addresses #f #f))
 
 (test-error-tail type-exception? (host-info #f))
 
