1cbdb1b6cf1140b91d8e3dcf2bd7a47683648774
Carlos-Ariel Vanegas <vanegascarlosar@gmail.com>
2026-07-13 14:55:30 -0400

    reverted rewrite for flonum tests
    The tests in 02-flonu have already been rewritten, and do no not need to be modeified further. this commit reverts changes made to those tests

--- tests/unit-tests/02-flonum/flatan.scm
+++ tests/unit-tests/02-flonum/flatan.scm
@@ -1,25 +1,43 @@
 (include "#.scm")
 
-(test-approximate 0. (##flatan 0.) 1e-12)
-(test-approximate .7853981633974483 (##flatan 1.) 1e-12)
-(test-approximate -.7853981633974483 (##flatan -1.) 1e-12)
+(test-approximate 0.0 (##flatan 0.0) 1e-12)
+(test-approximate 0.7853981633974483 (##flatan 1.0) 1e-12)
+(test-approximate -0.7853981633974483 (##flatan -1.0) 1e-12)
 
 (test-assert (flnan? (##flatan +nan.0)))
 
+#|
+
+If the implementation supports IEEE floating-point arithmetic (IEC 60559), 
+
+If y is +/-0 and x is negative or -0, +/-pi is returned
+If y is +/-0 and x is positive or +0, +/-0 is returned
+If y is +/-inf and x is finite, +/-pi/2 is returned
+If y is +/-inf and x is -inf, +/-3pi/4 is returned
+If y is +/-inf and x is +inf, +/-pi/4 is returned
+If x is +/-0 and y is negative, -pi/2 is returned
+If x is +/-0 and y is positive, +pi/2 is returned
+If x is -inf and y is finite and positive, +pi is returned
+If x is -inf and y is finite and negative, -pi is returned
+If x is +inf and y is finite and positive, +0 is returned
+If x is +inf and y is finite and negative, -0 is returned
+If either x is NaN or y is NaN, NaN is returned
+
+|#
 
 ;; If y is +/-0 and x is negative or -0, +/-pi is returned
 
-(test-approximate 3.141592653589793 (##flatan 0. -1.) 1e-12)
-(test-approximate 3.141592653589793 (##flatan 0. -0.) 1e-12)
+(test-approximate 3.141592653589793 (##flatan +0. -1.) 1e-12)
+(test-approximate 3.141592653589793 (##flatan +0. -0.) 1e-12)
 (test-approximate -3.141592653589793 (##flatan -0. -1.) 1e-12)
 (test-approximate -3.141592653589793 (##flatan -0. -0.) 1e-12)
 
 ;; If y is +/-0 and x is positive or +0, +/-0 is returned
 
-(test-approximate 0. (##flatan 0. 1.) 1e-12)
-(test-approximate 0. (##flatan 0. 0.) 1e-12)
-(test-approximate -0. (##flatan -0. 1.) 1e-12)
-(test-approximate -0. (##flatan -0. 0.) 1e-12)
+(test-approximate +0. (##flatan +0. +1.) 1e-12)
+(test-approximate +0. (##flatan +0. +0.) 1e-12)
+(test-approximate -0. (##flatan -0. +1.) 1e-12)
+(test-approximate -0. (##flatan -0. +0.) 1e-12)
 
 ;; If y is +/-inf and x is finite, +/-pi/2 is returned
 
@@ -33,12 +51,12 @@
 
 ;; If y is +/-inf and x is +inf, +/-pi/4 is returned
 
-(test-approximate .7853981633974483 (##flatan +inf.0 +inf.0) 1e-12)
-(test-approximate -.7853981633974483 (##flatan -inf.0 +inf.0) 1e-12)
+(test-approximate 0.7853981633974483 (##flatan +inf.0 +inf.0) 1e-12)
+(test-approximate -0.7853981633974483 (##flatan -inf.0 +inf.0) 1e-12)
 
 ;; If x is -inf and y is finite and positive, +pi is returned
 
-(test-approximate 3.141592653589793 (##flatan 1. -inf.0) 1e-12)
+(test-approximate 3.141592653589793 (##flatan 1.0 -inf.0) 1e-12)
 
 ;; If x is -inf and y is finite and negative, -pi is returned
 
@@ -46,7 +64,7 @@
 
 ;; If x is +inf and y is finite and positive, +0 is returned
 
-(test-approximate 0. (##flatan 1. +inf.0) 1e-12)
+(test-approximate +0. (##flatan +1. +inf.0) 1e-12)
 
 ;; If x is +inf and y is finite and negative, -0 is returned
 
@@ -58,25 +76,25 @@
 (test-assert (flnan? (##flatan 1. +nan.0)))
 
 
-(test-approximate 0. (flatan 0.) 1e-12)
-(test-approximate .7853981633974483 (flatan 1.) 1e-12)
-(test-approximate -.7853981633974483 (flatan -1.) 1e-12)
+(test-approximate 0.0 (flatan 0.0) 1e-12)
+(test-approximate 0.7853981633974483 (flatan 1.0) 1e-12)
+(test-approximate -0.7853981633974483 (flatan -1.0) 1e-12)
 
 (test-assert (flnan? (flatan +nan.0)))
 
 ;; If y is +/-0 and x is negative or -0, +/-pi is returned
 
-(test-approximate 3.141592653589793 (flatan 0. -1.) 1e-12)
-(test-approximate 3.141592653589793 (flatan 0. -0.) 1e-12)
+(test-approximate 3.141592653589793 (flatan +0. -1.) 1e-12)
+(test-approximate 3.141592653589793 (flatan +0. -0.) 1e-12)
 (test-approximate -3.141592653589793 (flatan -0. -1.) 1e-12)
 (test-approximate -3.141592653589793 (flatan -0. -0.) 1e-12)
 
 ;; If y is +/-0 and x is positive or +0, +/-0 is returned
 
-(test-approximate 0. (flatan 0. 1.) 1e-12)
-(test-approximate 0. (flatan 0. 0.) 1e-12)
-(test-approximate -0. (flatan -0. 1.) 1e-12)
-(test-approximate -0. (flatan -0. 0.) 1e-12)
+(test-approximate +0. (flatan +0. +1.) 1e-12)
+(test-approximate +0. (flatan +0. +0.) 1e-12)
+(test-approximate -0. (flatan -0. +1.) 1e-12)
+(test-approximate -0. (flatan -0. +0.) 1e-12)
 
 ;; If y is +/-inf and x is finite, +/-pi/2 is returned
 
@@ -90,12 +108,12 @@
 
 ;; If y is +/-inf and x is +inf, +/-pi/4 is returned
 
-(test-approximate .7853981633974483 (flatan +inf.0 +inf.0) 1e-12)
-(test-approximate -.7853981633974483 (flatan -inf.0 +inf.0) 1e-12)
+(test-approximate 0.7853981633974483 (flatan +inf.0 +inf.0) 1e-12)
+(test-approximate -0.7853981633974483 (flatan -inf.0 +inf.0) 1e-12)
 
 ;; If x is -inf and y is finite and positive, +pi is returned
 
-(test-approximate 3.141592653589793 (flatan 1. -inf.0) 1e-12)
+(test-approximate 3.141592653589793 (flatan 1.0 -inf.0) 1e-12)
 
 ;; If x is -inf and y is finite and negative, -pi is returned
 
@@ -103,7 +121,7 @@
 
 ;; If x is +inf and y is finite and positive, +0 is returned
 
-(test-approximate 0. (flatan 1. +inf.0) 1e-12)
+(test-approximate +0. (flatan +1. +inf.0) 1e-12)
 
 ;; If x is +inf and y is finite and negative, -0 is returned
 
@@ -115,8 +133,8 @@
 (test-assert (flnan? (flatan 1. +nan.0)))
 
 (test-error-tail wrong-number-of-arguments-exception? (flatan))
-(test-error-tail wrong-number-of-arguments-exception? (flatan 1. 2. 3.))
+(test-error-tail wrong-number-of-arguments-exception? (flatan 1.0 2.0 3.0))
 
 (test-error-tail type-exception? (flatan 1))
-(test-error-tail type-exception? (flatan 1 2.))
-(test-error-tail type-exception? (flatan 1. 2))
+(test-error-tail type-exception? (flatan 1 2.0))
+(test-error-tail type-exception? (flatan 1.0 2))
--- tests/unit-tests/02-flonum/flhypot.scm
+++ tests/unit-tests/02-flonum/flhypot.scm
@@ -7,13 +7,25 @@
 
 (test-eqv 5. (##flhypot 3. 4.))
 
+#|
+
+C99 rules:
+
+If the implementation supports IEEE floating-point arithmetic (IEC 60559),
+
+hypot(x, y), hypot(y, x), and hypot(x, -y) are equivalent
+if one of the arguments is +/-0, hypot is equivalent to fabs called with the non-zero argument
+if one of the arguments is +/-inf, hypot returns +inf even if the other argument is NaN
+otherwise, if any of the arguments is NaN, NaN is returned 
+
+|#
 
 ;; if one of the arguments is +/-0, hypot is equivalent to fabs called with the non-zero argument
 
-(test-eqv 1. (##flhypot 0. -1.))
-(test-eqv 0. (##flhypot 0. -0.))
-(test-eqv 1. (##flhypot -0. -1.))
-(test-eqv 0. (##flhypot -0. -0.))
+(test-eqv +1. (##flhypot +0. -1.))
+(test-eqv +0. (##flhypot +0. -0.))
+(test-eqv +1. (##flhypot -0. -1.))
+(test-eqv +0. (##flhypot -0. -0.))
 
 ;; if one of the arguments is +/-inf, hypot returns +inf even if the other argument is NaN
 
@@ -36,10 +48,10 @@
 
 ;; if one of the arguments is +/-0, hypot is equivalent to fabs called with the non-zero argument
 
-(test-eqv 1. (flhypot 0. -1.))
-(test-eqv 0. (flhypot 0. -0.))
-(test-eqv 1. (flhypot -0. -1.))
-(test-eqv 0. (flhypot -0. -0.))
+(test-eqv +1. (flhypot +0. -1.))
+(test-eqv +0. (flhypot +0. -0.))
+(test-eqv +1. (flhypot -0. -1.))
+(test-eqv +0. (flhypot -0. -0.))
 
 ;; if one of the arguments is +/-inf, hypot returns +inf even if the other argument is NaN
 
@@ -58,8 +70,8 @@
 (test-assert (flnan? (flhypot 1. +nan.0)))
 
 (test-error-tail wrong-number-of-arguments-exception? (flhypot))
-(test-error-tail wrong-number-of-arguments-exception? (flhypot 1.))
-(test-error-tail wrong-number-of-arguments-exception? (flhypot 1. 2. 3.))
+(test-error-tail wrong-number-of-arguments-exception? (flhypot 1.0))
+(test-error-tail wrong-number-of-arguments-exception? (flhypot 1.0 2.0 3.0))
 
-(test-error-tail type-exception? (flhypot 1 2.))
-(test-error-tail type-exception? (flhypot 1. 2))
+(test-error-tail type-exception? (flhypot 1 2.0))
+(test-error-tail type-exception? (flhypot 1.0 2))
